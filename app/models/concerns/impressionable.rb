874e9c12b8c0917e2067582392e71efec40f07d6
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-06-19 20:22:05 +0530

    Added tasks to documents

--- app/models/concerns/impressionable.rb
+++ app/models/concerns/impressionable.rb
@@ -4,9 +4,13 @@ module Impressionable
   included do
     is_impressionable counter_cache: true, unique: :user_id
 
-    def viewed_by
+    def viewed_by(entity_id: nil)
       ids = impressions.pluck(:user_id).uniq
-      User.where(id: ids)
+      if entity_id
+        User.where(id: ids, entity_id:)
+      else
+        User.where(id: ids)
+      end
     end
   end
 end
