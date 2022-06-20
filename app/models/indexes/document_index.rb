918594eaab067379221549d974c5158d08e06343
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-06-20 14:34:41 +0530

    Fixed index properties

--- app/models/indexes/document_index.rb
+++ app/models/indexes/document_index.rb
@@ -1,15 +1,15 @@
 class DocumentIndex < Chewy::Index
-  SEARCH_FIELDS = %i[name folder_name entity_name tags properties].freeze
+  SEARCH_FIELDS = %i[name folder_name entity_name tag_list properties].freeze
 
   index_scope Document.includes(:entity, :folder, tags: :taggings)
 
   field :name
-  field :tags
+  field :tag_list
   field :entity_id
   field :entity_name, value: ->(doc) { doc.entity.name if doc.entity }
   field :file_file_name
   field :folder_id
-  field :properties
+  field :properties, value: ->(doc) { doc.properties.to_json if doc.properties }
   field :folder_name, value: ->(doc) { doc.folder.name if doc.folder }
   field :folder_full_path, value: ->(doc) { doc.folder.full_path if doc.folder }
   field :created_at, type: "date"
--- app/models/indexes/investor_index.rb
+++ app/models/indexes/investor_index.rb
@@ -1,13 +1,13 @@
 class InvestorIndex < Chewy::Index
-  SEARCH_FIELDS = %i[investor_name investee_name category tags city properties].freeze
+  SEARCH_FIELDS = %i[investor_name investee_name category tag_list city properties].freeze
 
   index_scope Investor.includes(:investor_entity, :entity, tags: :taggings)
   field :investor_name
   field :investee_name
   field :category
   field :city
-  field :tags
-  field :properties
+  field :tag_list
+  field :properties, value: ->(doc) { doc.properties.to_json if doc.properties }
   field :entity_id
   field :is_holdings_entity
   field :investor_access_count
