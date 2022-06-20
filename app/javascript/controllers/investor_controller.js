f5f6d82e53161a8f34d0b6e2a9a4a0d828abc30f
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-06-20 16:31:30 +0530

    Backup

--- app/javascript/controllers/investor_controller.js
+++ app/javascript/controllers/investor_controller.js
@@ -4,13 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    $(".select2").on('select2:select', function (e) { 
-      if(e.params.data.text.length > 1) {
-        $("#investor_name_group").hide();
-      } else {
-        $("#investor_name_group").show();
-      }
-    });
+    
   }
 
 }
