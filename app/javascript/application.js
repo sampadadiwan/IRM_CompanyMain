4ea027af96a77e2fc5cfe7f8f456f75c2c769caf
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-06-24 19:21:28 +0530

    Misc changes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -103,5 +103,28 @@ $( document ).on('turbo:load', function() {
       e.preventDefault();
     });
 
+    $('#jstree').jstree({
+      "core" : {
+        "themes" : {
+          "variant" : "large"
+        }
+      },
+      "checkbox" : {
+        "keep_selected_style" : false
+      },
+      "plugins" : [ "wholerow", "checkbox" ]
+    });
+    
+    $("#tree_view").jstree({ "plugins" : ["themes","html_data","ui"] });
+    $("#tree_view li").on("click", "a", 
+        function() {
+            document.location.href = this;
+        }
+    );
+
+    $("#tree_view").jstree("open_all")
+    
+ 
+    console.log("jstree initialized");
     
 });
--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -5,6 +5,11 @@ export default class extends Controller {
     var urlParams = new URLSearchParams(window.location.search);
     let tab = urlParams.get('tab');
     console.log(`tab_controller switching to #${tab}`);
-    $(`a[href="#${tab}"]`).click() // Select tab by name
+    $(`a[href="#${tab}"]`).click();
+    
+    if( $(`#${tab} .load_data_link`).length > 0 ) {
+      $(`#${tab} .load_data_link`).find('span').trigger('click'); // Works
+      $(`#${tab} .load_data_link`).hide();  // Select tab by name
+    }
   }
 }
