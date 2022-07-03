d78cf0ceceb94da848fe1bcdf845ae00d29fc00d
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-07-03 18:26:13 +0530

    Added calc for vested options

--- app/models/holding_summary.rb
+++ app/models/holding_summary.rb
@@ -15,7 +15,9 @@ class HoldingSummary
 
     @holdings.each do |holding|
       per_share_value = holding.entity.valuations.last.per_share_value
-      profits += ((per_share_value * price_growth) - holding.option_pool.excercise_price) * holding.quantity
+
+      quantity = @params[:all_or_vested] == "Vested" ? holding.vested_quantity : holding.quantity
+      profits += ((per_share_value * price_growth) - holding.option_pool.excercise_price) * quantity
     end
 
     profits
@@ -29,20 +31,44 @@ class HoldingSummary
     tax_rate = @params[:tax_rate] ? @params[:tax_rate].to_f : 30
 
     if last_valuation
-      estimated_profit = estimated_profits(price_growth)
-      estimated_taxes = estimated_profit * tax_rate / 100
-      total_value_cents = @user.holdings.options.where(entity_id: @entity.id).sum(:value_cents) / 100
-      esop_value = Money.new(total_value_cents, @entity.currency)
-
-      cost_neutral_sale = (estimated_taxes + esop_value) / (last_valuation * price_growth)
+      estimated_taxes, estimated_profit, cost_neutral_sale, last_valuation, esop_value =
+        from_last_valuation(last_valuation)
     else
-      estimated_taxes = Money.new(0, @entity.currency)
-      estimated_profit = Money.new(0, @entity.currency)
-      cost_neutral_sale = 0
-      last_valuation = Money.new(0, @entity.currency)
+      estimated_taxes, estimated_profit, cost_neutral_sale, last_valuation, esop_value =
+        no_valuation
     end
 
-    [price_growth, tax_rate, estimated_taxes, estimated_profit, cost_neutral_sale, last_valuation]
+    [price_growth, tax_rate, estimated_taxes, estimated_profit, cost_neutral_sale, last_valuation, esop_value]
+  end
+
+  def from_last_valuation(last_valuation)
+    # Get the price growth from the UI
+    price_growth = @params[:price_growth].present? ? @params[:price_growth].to_f : 3
+    # Get the tax rate from the UI
+    tax_rate = @params[:tax_rate] ? @params[:tax_rate].to_f : 30
+
+    estimated_profit = estimated_profits(price_growth)
+    estimated_taxes = estimated_profit * tax_rate / 100
+    options = @user.holdings.options.where(entity_id: @entity.id)
+    total_value_cents = if @params[:all_or_vested] == "Vested"
+                          options.inject(0) { |sum, h| sum + (h.vested_quantity * h.price_cents) }
+                        else
+                          options.sum(:value_cents)
+                        end
+    esop_value = Money.new(total_value_cents, @entity.currency)
+
+    cost_neutral_sale = (estimated_taxes + esop_value) / (last_valuation * price_growth)
+    [estimated_taxes, estimated_profit, cost_neutral_sale, last_valuation, esop_value]
+  end
+
+  def no_valuation
+    estimated_taxes = Money.new(0, @entity.currency)
+    estimated_profit = Money.new(0, @entity.currency)
+    cost_neutral_sale = 0
+    last_valuation = Money.new(0, @entity.currency)
+    esop_value = Money.new(0, @entity.currency)
+
+    [estimated_taxes, estimated_profit, cost_neutral_sale, last_valuation, esop_value]
   end
 
   def projected_profits
