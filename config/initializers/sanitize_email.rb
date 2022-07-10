5982fa6ab52ffdde4b65ea5f2f6f5f43fb8b177d
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-07-10 21:42:09 +0530

    Removed error emails from sanitization

--- config/initializers/sanitize_email.rb
+++ config/initializers/sanitize_email.rb
@@ -3,7 +3,9 @@ SanitizeEmail::Config.configure do |config|
   # config[:sanitized_cc] =         'cc@sanitize_email.org'
   # config[:sanitized_bcc] =        'bcc@sanitize_email.org'
   # run/call whatever logic should turn sanitize_email on and off in this Proc:
-  config[:activation_proc] = proc { %w[development staging sandbox production].include?(Rails.env) }
+  config[:activation_proc] = proc { |message|
+      %w[development staging sandbox production].include?(Rails.env) && message.subject.index("Error") == nil
+  }
   # config[:use_actual_email_prepended_to_subject] = true         # or false
   # config[:use_actual_environment_prepended_to_subject] = true   # or false
   # config[:use_actual_email_as_sanitized_user_name] = true       # or false
