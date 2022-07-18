3cf94b8a5081dfae1e3194c1cc60bad1fa0628d1
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-07-18 11:16:50 +0530

    Added approval response status

--- app/models/approval_response.rb
+++ app/models/approval_response.rb
@@ -4,4 +4,7 @@ class ApprovalResponse < ApplicationRecord
   belongs_to :response_user, class_name: "User"
   belongs_to :approval
   has_rich_text :details
+
+  counter_culture :approval, column_name: proc { |resp| resp.status == 'Approved' ? 'approved_count' : nil }
+  counter_culture :approval, column_name: proc { |resp| resp.status == 'Rejected' ? 'rejected_count' : nil }
 end
