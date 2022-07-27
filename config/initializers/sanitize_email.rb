944e51576b10d0644649af0d870090c54eca520c
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-07-27 16:36:39 +0530

    Added sandbox to staging

--- config/initializers/sanitize_email.rb
+++ config/initializers/sanitize_email.rb
@@ -4,7 +4,7 @@ SanitizeEmail::Config.configure do |config|
   # config[:sanitized_bcc] =        'bcc@sanitize_email.org'
   # run/call whatever logic should turn sanitize_email on and off in this Proc:
   config[:activation_proc] = proc { |message|
-    %w[development staging].include?(Rails.env) && message.subject.index("Error").nil?
+    %w[development].include?(Rails.env) && message.subject.index("Error").nil?
   }
   # config[:use_actual_email_prepended_to_subject] = true         # or false
   config[:use_actual_environment_prepended_to_subject] = true   # or false
