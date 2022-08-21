7da26194760b14d249a3336c2df45adffbdf1503
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-08-21 11:31:42 +0530

    Upply file upload supports all types now

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -1,5 +1,5 @@
 class FileUploader < Shrine
-  ALLOWED_TYPES = %w[image/* application/* video/*].freeze
+  ALLOWED_TYPES = %w[*/*].freeze
   Attacher.validate do
     # validate_mime_type ALLOWED_TYPES
     validate_max_size 500 * 1024 * 1024, message: "is too large (max is 500 MB)"
