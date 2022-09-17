b971fb5cf3432cda84a97803d97f898d3fd554fc
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-09-17 13:20:50 +0530

    New computation for cap table based on perferred conversion

--- db/migrate/20220915155310_add_pref_conversion_to_investment.rb
+++ db/migrate/20220915155310_add_pref_conversion_to_investment.rb
@@ -1,5 +1,6 @@
 class AddPrefConversionToInvestment < ActiveRecord::Migration[7.0]
   def change
     add_column :investments, :preferred_conversion, :integer
+    Investment.update_all(preferred_conversion: 1)
   end
 end
