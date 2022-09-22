c583bad346520eb910a847559f18b3b05b261286
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-09-22 19:27:38 +0530

    Refactored to packs - more

--- config/application.rb
+++ config/application.rb
@@ -26,28 +26,32 @@ module InvestorRelationshipManagement
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("indices")
 
-    core_view_paths = %w[access_rights documents entities folders notes permissions reminders tasks users investors investor_accesses valuations form_types form_custom_fields approvals payments nudges]
+    core_view_paths = %w[access_rights documents entities folders notes permissions reminders tasks users investors investor_accesses valuations form_types form_custom_fields approvals payments nudges impoer_uploads misc]
 
     core_view_paths.each do |view_path|
       config.paths['app/views'].unshift("#{Rails.root}/app/packs/core/#{view_path}/views")
+      config.paths['app/helpers'].unshift("#{Rails.root}/app/packs/core/#{view_path}/helpers")
     end
 
     funds_view_paths = %w[funds capital_calls capital_commitments capital_distributions capital_distribution_payments capital_remittances investment_opportunities]
 
     funds_view_paths.each do |view_path|
       config.paths['app/views'].unshift("#{Rails.root}/app/packs/funds/#{view_path}/views")
+      config.paths['app/helpers'].unshift("#{Rails.root}/app/packs/funds/#{view_path}/helpers")
     end
 
     secondaries_view_paths = %w[secondary_sales offers interests]
 
     secondaries_view_paths.each do |view_path|
       config.paths['app/views'].unshift("#{Rails.root}/app/packs/secondaries/#{view_path}/views")
+      config.paths['app/helpers'].unshift("#{Rails.root}/app/packs/secondaries/#{view_path}/helpers")
     end
 
     startup_view_paths = %w[investments aggregate_investments option_pools excercises deals funding_rounds]
 
     startup_view_paths.each do |view_path|
       config.paths['app/views'].unshift("#{Rails.root}/app/packs/startups/#{view_path}/views")
+      config.paths['app/helpers'].unshift("#{Rails.root}/app/packs/startups/#{view_path}/helpers")
     end
 
     config.to_prepare do
