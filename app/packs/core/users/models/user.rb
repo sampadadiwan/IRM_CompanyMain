2da24bd3eaa5d231178bd8b8a88331e3129a67fb
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-09-23 09:31:03 +0530

    Added reminders to funds

--- app/packs/core/users/models/user.rb
+++ app/packs/core/users/models/user.rb
@@ -1,37 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id                     :integer          not null, primary key
-#  first_name             :string(255)
-#  last_name              :string(255)
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#  email                  :string(255)      default(""), not null
-#  encrypted_password     :string(255)      default(""), not null
-#  reset_password_token   :string(255)
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  phone                  :string(100)
-#  active                 :boolean          default("1")
-#  confirmation_token     :string(255)
-#  confirmed_at           :datetime
-#  confirmation_sent_at   :datetime
-#  entity_id              :integer
-#  deleted_at             :datetime
-#  system_created         :boolean          default("0")
-#  sign_in_count          :integer          default("0"), not null
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string(255)
-#  last_sign_in_ip        :string(255)
-#  accept_terms           :boolean          default("0")
-#  whatsapp_enabled       :boolean          default("0")
-#  sale_notification      :boolean          default("0")
-#  curr_role              :string(20)
-#  permissions            :integer          default("0"), not null
-#
-
 class User < ApplicationRecord
   include PublicActivity::Model
 
