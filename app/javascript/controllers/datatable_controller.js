8afb56e9969a1c9c687d628e9058f7e963ecb6ca
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-10-14 16:34:15 +0530

    Better support for back button and jqDataTables

--- app/javascript/controllers/datatable_controller.js
+++ app/javascript/controllers/datatable_controller.js
@@ -3,20 +3,38 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
 
-      let table = $('.jqDataTable').DataTable({        
-        stateSave: false,
-        retrieve: true,
-        language: {
-          search: '',
-          searchPlaceholder: "Search...",
-          paginate: {
-            "previous": "Prev"
-          }          
+      let table = {};
+
+      $.each( $('.jqDataTable'), function( key, value ) {
+        // console.log( key + ": " + value );
+        if ( $.fn.dataTable.isDataTable( value ) ) {
         }
-      });   
+        else {
+          table[key] = $(value).DataTable({        
+            stateSave: false,
+            retrieve: true,
+            language: {
+              search: '',
+              searchPlaceholder: "Search...",
+              paginate: {
+                "previous": "Prev"
+              }          
+            }
+          });
+  
+        }
+        
+      });
+
+      
+      
       // Ensure DataTable is destroyed, else it gets duplicated
-      $(document).on('turbo:before-cache', function() {     
-        table.destroy();
+      $(document).on('turbo:before-cache', function() {    
+        $.each( table, function( key, value ) {
+          if ( $.fn.dataTable.isDataTable( value ) ) { 
+            value.destroy();
+          }
+        });
       });
       
       let searchTerm = $("#search_term");
