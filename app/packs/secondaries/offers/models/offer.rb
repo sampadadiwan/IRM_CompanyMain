e6cccecfb0aa09a91af498bcd0827ab86a5908ce
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-11-03 07:43:19 +0530

    Now displays buyer_entity_name for allocation

--- app/packs/secondaries/offers/models/offer.rb
+++ app/packs/secondaries/offers/models/offer.rb
@@ -56,8 +56,6 @@ class Offer < ApplicationRecord
 
   validates :full_name, :address, :PAN, :bank_account_number, :ifsc_code, presence: true, if: proc { |o| o.secondary_sale.finalized }
 
-  # validates :address_proof, :id_proof, :signature, presence: true if Rails.env.production?
-
   validate :check_quantity
   validate :sale_active, on: :create
 
