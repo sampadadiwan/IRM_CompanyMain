49dd5c930cd101b705e65b9ffcbbbac03c6ebf7e
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-11-09 06:54:38 +0530

    Added DSC to pdf documents, not tested

--- app/javascript/controllers/pdf_controller.js
+++ app/javascript/controllers/pdf_controller.js
@@ -5,33 +5,81 @@ export default class extends Controller {
     viewer_instance;
 
     async save_signed() {
-        
-                const { documentViewer, annotationManager } = this.viewer_instance.Core;
 
-                let doc = documentViewer.getDocument();
-                let xfdfString = await annotationManager.exportAnnotations();
-                let data = await doc.getFileData({
-                    // saves the document with annotations in it
-                    xfdfString
-                });
-                let arr = new Uint8Array(data);
-                let blob = new Blob([arr], { type: 'application/pdf' });
+        const { documentViewer, annotationManager } = this.viewer_instance.Core;
 
-                console.log(blob);
+        let doc = documentViewer.getDocument();
+        let xfdfString = await annotationManager.exportAnnotations();
+        let data = await doc.getFileData({
+            // saves the document with annotations in it
+            xfdfString
+        });
+        let arr = new Uint8Array(data);
+        let blob = new Blob([arr], { type: 'application/pdf' });
 
-                let file = new File([blob], "signed.pdf",{type:"application/pdf", lastModified:new Date().getTime()});
+        console.log(blob);
 
-                let container = new DataTransfer();
-                container.items.add(file);
+        this.saveBlob(blob);
 
-                let fileInputElement = document.getElementById('document_file');
-
-                fileInputElement.files = container.files;
-                $("#signed_doc_form").submit();
-
-        
     }
-    
+
+    saveBlob(blob) {
+        let file = new File([blob], "signed.pdf", { type: "application/pdf", lastModified: new Date().getTime() });
+
+        let container = new DataTransfer();
+        container.items.add(file);
+
+        let fileInputElement = document.getElementById('document_file');
+
+        fileInputElement.files = container.files;
+        $("#signed_doc_form").submit();
+    }
+
+
+    base64ToArrayBuffer(base64) {
+        var binary_string = window.atob(base64);
+        var len = binary_string.length;
+        var bytes = new Uint8Array(len);
+        for (let i = 0; i < len; i++) {
+            bytes[i] = binary_string.charCodeAt(i);
+        }
+        return bytes.buffer;
+    }
+
+    async readDscFile(e) {
+        var fileInput = document.getElementById('dscfile');
+
+        var reader = new FileReader();
+        reader.readAsDataURL(fileInput.files[0]);
+
+        const base64ToArrayBuffer = this.base64ToArrayBuffer;
+        const toDsc = this.dsc;
+        const viewer_instance = this.viewer_instance;
+        const saveBlob = this.saveBlob;
+        const createSignatureField = this.createSignatureField;
+
+        reader.onload = function () {
+
+            // Use a regex to remove data url part
+            const base64String = reader.result
+                .replace('data:', '')
+                .replace(/^.+,/, '');
+
+            // console.log(base64String);
+            let arraybuffer = base64ToArrayBuffer(base64String);
+            console.log(arraybuffer);
+
+            createSignatureField(viewer_instance);
+            toDsc(arraybuffer, viewer_instance, saveBlob);
+        };
+        reader.onerror = function (error) {
+            console.log('Error: ', error);
+        };
+
+
+        e.preventDefault();
+    }
+
 
     connect() {
 
@@ -59,58 +107,48 @@ export default class extends Controller {
 
     viewPDF(viewer_link, viewer_watermark) {
         WebViewer({
+            fullAPI: true,
             licenseKey: 'CapHive Private Limited (altconnects.com):OEM:CapHive::B+:AMS(20230928):70A55F8D0437F80AF360B13AC982537860613F8D8776BD3B95853BA45A955E6D54F2B6F5C7',
-            path: '/lib', 
+            path: '/lib',
         }, document.getElementById('viewer'))
-            .then(instance => {
+            .then(async (instance) => {
                 this.viewer_instance = instance;
+                const { PDFNet, documentViewer } = instance.Core;
+                await PDFNet.initialize();
+                // const docViewer = instance.Core.documentViewer;
+                // const annotManager = instance.Core.annotationManager;
+
+
                 instance.UI.disableElements(['toolbarGroup-Shapes']);
                 instance.UI.disableElements(['toolbarGroup-View']);
                 instance.UI.disableElements(['toolbarGroup-Edit']);
                 instance.UI.disableElements(['toolbarGroup-Annotate']);
                 instance.UI.disableElements(['toolbarGroup-Forms']);
                 instance.UI.disableElements(['toolbarGroup-FillAndSign']);
-                
-                
-                
 
-                instance.UI.openElements([ 'menuOverlay' ]);
-                if($("#download_document").val() !== "true") { 
-                    instance.UI.disableElements([ 'downloadButton' ]);
-                }
-                if($("#printing_document").val() !== "true") { 
-                    instance.UI.disableElements([ 'printButton' ]);
-                }
 
-                if($("#sign_document").val() !== "true") { 
-                    instance.UI.disableElements(['ribbons']);
-                    instance.UI.disableElements(['toolsHeader']);            
-                    instance.UI.disableElements([ 'toolbarGroup-Insert' ]);
-                } else {
-                    instance.UI.openElements([ 'toolbarGroup-Insert' ]);
-                    instance.UI.setHeaderItems(function(header) {
-                        header.getHeader('toolbarGroup-Insert').delete(2);
-                        header.getHeader('toolbarGroup-Insert').delete(3);
-                        header.getHeader('toolbarGroup-Insert').delete(4);
-                    });
+
+
+                instance.UI.openElements(['menuOverlay']);
+                if ($("#download_document").val() !== "true") {
+                    instance.UI.disableElements(['downloadButton']);
+                }
+                if ($("#printing_document").val() !== "true") {
+                    instance.UI.disableElements(['printButton']);
                 }
 
+                
 
-                const docViewer = instance.Core.documentViewer;
-                const annotManager = instance.Core.annotationManager;
 
                 instance.UI.loadDocument(viewer_link, {
                     filename: 'myfile.pdf'
                 });
 
                 instance.UI.setFitMode(instance.FitMode.FitWidth);
-                
+
                 $("#viewer_label").hide();
                 $("#viewer").show();
-        
 
-                const { documentViewer } = instance.Core;
-                
 
                 documentViewer.setWatermark({
                     // Draw diagonal watermark in middle of the document
@@ -137,6 +175,24 @@ export default class extends Controller {
                 $(".document_download_icon").hide();
                 console.log(`In full screen ${instance.UI.isFullscreen()}`);
 
+                if ($("#sign_document").val() !== "true") {
+                    console.log(`############# Signing is NOT turned on`);
+
+                    instance.UI.disableElements(['ribbons']);
+                    instance.UI.disableElements(['toolsHeader']);
+                    instance.UI.disableElements(['toolbarGroup-Insert']);
+
+                } else {
+                    console.log(`############# Signing is turned on`);
+                    instance.UI.openElements(['toolbarGroup-Insert']);
+                    instance.UI.setHeaderItems(function (header) {
+                        header.getHeader('toolbarGroup-Insert').delete(2);
+                        header.getHeader('toolbarGroup-Insert').delete(3);
+                        header.getHeader('toolbarGroup-Insert').delete(4);
+                    });
+                }
+
+
             });
     }
 
@@ -146,21 +202,10 @@ export default class extends Controller {
 
     initCompleted() {
         console.log("initCompleted");
-        $('button[data-element="signatureToolGroupButton"]').click();
     }
 
     officeToPDF(viewer_link, viewer_watermark) {
-        
         this.convertOfficeToPDF(viewer_link, `converted.pdf`, viewer_watermark);
-
-        // PDFNet.initialize()
-        //     .then(() =>  {
-        //         this.convertOfficeToPDF(viewer_link, `converted.pdf`, viewer_watermark)                
-        //      })            
-        //     .catch(err => {
-        //         console.log('An error was encountered! :(', err);
-        //         $(".document_download_icon").show();
-        //     });
     }
 
     convertOfficeToPDF(inputUrl, outputName, viewer_watermark, l) {
@@ -174,74 +219,112 @@ export default class extends Controller {
         });
     }
 
-    dsc() {
 
-        const { PDFNet, documentViewer } = this.viewer_instance.Core;
+    async createSignatureField(viewer_instance) {
+        const { PDFNet, documentViewer } = viewer_instance.Core;
+        const doc = await documentViewer.getDocument().getPDFDoc();
+        console.log(`Got DOC ${doc}`);
+        await PDFNet.runWithCleanup(async () => {
 
-          
-        documentViewer.addEventListener('documentLoaded', async () => {
-            await PDFNet.initialize();
-            const doc = await documentViewer.getDocument().getPDFDoc();
-        
-            // Run PDFNet methods with memory management
-            await PDFNet.runWithCleanup(async () => {
-        
             // lock the document before a write operation
             // runWithCleanup will auto unlock when complete
             doc.lock();
-        
+            console.log("Document locked");
+            
+            const certificationFieldName = "SigField";
+
+            const certificationSigField = await doc.createDigitalSignatureField(certificationFieldName);
+            console.log(certificationSigField);
+        });
+    }
+    async dsc(dsc_signature_buffer, viewer_instance, saveBlob) {
+
+        console.log("dsc called");
+
+        const { PDFNet, documentViewer } = viewer_instance.Core;
+
+        await PDFNet.initialize();
+        console.log(`PDFNet.initialized`);
+
+        const doc = await documentViewer.getDocument().getPDFDoc();
+        console.log(`got doc ${doc}`);
+
+
+        // Run PDFNet methods with memory management
+        await PDFNet.runWithCleanup(async () => {
+
+            // lock the document before a write operation
+            // runWithCleanup will auto unlock when complete
+            doc.lock();
+            console.log("Document locked");
+
             // Add an StdSignatureHandler instance to PDFDoc, making sure to keep track of it using the ID returned.
-            const sigHandlerId = await doc.addStdSignatureHandlerFromURL(cert_file_path, 'password');
-        
+            const sigHandlerId = await doc.addStdSignatureHandlerFromBuffer(dsc_signature_buffer, 'isb2005*');
+
+            console.log("Added sigHandlerId");
+
             /**
              * Certifying a document requires a digital signature field (can optionally be
              * hidden), hence why the logic below either looks for an existing field in the
              * document, or suggests creating a field in the document
              */
-        
+
             // Retrieve the unsigned certification signature field.
             /**
              * Note: Replace certificationFieldName with the field name in the
              * document that is being certified
              */
+
+            const certificationFieldName = "SigField";
+
             const foundCertificationField = await doc.getField(certificationFieldName);
             const certificationSigField = await PDFNet.DigitalSignatureField.createFromField(foundCertificationField);
-        
+
             // Alternatively, create a new signature form field in the PDFDoc. The name argument is optional;
             // leaving it empty causes it to be auto-generated. However, you may need the name for later.
             // Acrobat doesn't show digsigfield in side panel if it's without a widget. Using a
             // Rect with 0 width and 0 height, or setting the NoPrint/Invisible flags makes it invisible.
             // const certificationSigField = await doc.createDigitalSignatureField(certificationFieldName);
-        
+            console.log(certificationSigField);
+
             // (OPTIONAL) Add more information to the signature dictionary.
-            await certificationSigField.setLocation("Vancouver, BC");
-            await certificationSigField.setReason("Document certification.");
-            await certificationSigField.setContactInfo("www.pdftron.com");
-        
+            // await certificationSigField.setLocation("Vancouver, BC");
+            // await certificationSigField.setReason("Document certification.");
+            // await certificationSigField.setContactInfo("www.pdftron.com");
+            // Prepare the document locking permission level to be applied upon document certification.
+            // certificationSigField.setDocumentPermissions(PDFNet.DigitalSignatureField.DocumentPermissions.e_no_changes_allowed);
+
+            console.log('added location to certificationSigField');
             // (OPTIONAL) Add an appearance to the signature field.
-            const img = await PDFNet.Image.createFromURL(doc, appearance_img_path);
+            // const img = await PDFNet.Image.createFromURL(doc, "/img/ST041.gif");
+
+            // console.log("Created image for signature");
+
             const certifySignatureWidget = await PDFNet.SignatureWidget.createWithDigitalSignatureField(
                 doc,
                 await PDFNet.Rect.init(0, 100, 200, 150),
                 certificationSigField
-                );
-            await certifySignatureWidget.createSignatureAppearance(img);
+            );
+
+            console.log("Created certifySignatureWidget");
+            // await certifySignatureWidget.createSignatureAppearance(img);
             const page1 = await doc.getPage(1);
             page1.annotPushBack(certifySignatureWidget);
-        
-            // Prepare the document locking permission level to be applied upon document certification.
-            certificationSigField.setDocumentPermissions(PDFNet.DigitalSignatureField.DocumentPermissions.e_no_changes_allowed);
-        
+
+
             // Prepare the signature and signature handler for certification.
             await certificationSigField.certifyOnNextSaveWithCustomHandler(sigHandlerId);
-        
+
             // The actual certification signing will be done during the save operation.
             const buf = await doc.saveMemoryBuffer(0);
             const blob = new Blob([buf], { type: 'application/pdf' });
-            saveAs(blob, 'certified_doc.pdf');
-            });
-        })
-          
+            console.log(blob);
+            // saveAs(blob, 'certified_doc.pdf');
+
+            saveBlob(blob);
+        });
+        
+
     }
 
 
