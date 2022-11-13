eda61286bc79ec94e58310a65a1dd79ea03ba78d
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-11-13 18:08:45 +0530

    Added realtime currency formatter

--- app/javascript/controllers/currencyformat_controller.js
+++ app/javascript/controllers/currencyformat_controller.js
@@ -6,14 +6,16 @@ export default class extends Controller {
     
   }
 
-  format() {
-    const input = document.querySelector("#currency");
-    const [label] = input.labels;
-    const formatter = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"});
+  format(e) {
+    const input = e.target;
+    const formatter = new Intl.NumberFormat("en-US");
+    let span = $(e.target).closest(".form-group").find(".formatted_currency");
+    if(input.value) {
+        span.text(formatter.format(input.value));
+    } else {
+        span.text("");
+    }
 
-    input.addEventListener("input", e => {
-      label.textContent = formatter.format(input.value)
-    })
   }
 
 
