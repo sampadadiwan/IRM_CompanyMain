cea11c344552a22d5501503f4d333d79c49f7071
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-11-20 16:23:37 +0530

    NGROK_HOST not used in non development env

--- app/packs/core/base/controllers/application_controller.rb
+++ app/packs/core/base/controllers/application_controller.rb
@@ -72,9 +72,16 @@ class ApplicationController < ActionController::Base
   end
 
   def set_current_entity
-    if request.subdomain.present? && ENV['NGROK_HOST'].exclude?(request.subdomain) && !ENV['HOST'].starts_with?(request.subdomain)
-      @current_entity = Entity.where(sub_domain: request.subdomain).load_async.first
-      redirect_to(ENV['BASE_URL'], allow_other_host: true) unless @current_entity
+    if Rail.development?
+      if request.subdomain.present? && ENV['NGROK_HOST'].exclude?(request.subdomain) && !ENV['HOST'].starts_with?(request.subdomain)
+        @current_entity = Entity.where(sub_domain: request.subdomain).load_async.first
+        redirect_to(ENV['BASE_URL'], allow_other_host: true) unless @current_entity
+      end
+    else
+      if request.subdomain.present? && !ENV['HOST'].starts_with?(request.subdomain)
+        @current_entity = Entity.where(sub_domain: request.subdomain).load_async.first
+        redirect_to(ENV['BASE_URL'], allow_other_host: true) unless @current_entity
+      end
     end
   end
 end
