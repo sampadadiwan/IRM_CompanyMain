19fa409f62a3a4bf1e615a6a286df99dbba9dcb0
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-11-22 18:11:04 +0530

    Removed strict loading for IA

--- app/packs/core/investor_accesses/models/investor_access.rb
+++ app/packs/core/investor_accesses/models/investor_access.rb
@@ -12,7 +12,7 @@ class InvestorAccess < ApplicationRecord
   belongs_to :investor # , strict_loading: true
   counter_culture :investor, column_name: proc { |model| model.approved ? 'investor_access_count' : 'unapproved_investor_access_count' }
 
-  belongs_to :user, optional: true, strict_loading: true
+  belongs_to :user, optional: true #, strict_loading: true
   belongs_to :granter, class_name: "User", foreign_key: :granted_by, optional: true
 
   delegate :name, to: :entity, prefix: :entity
