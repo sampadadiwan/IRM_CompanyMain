479f4c56adfb50bb19fcf8204db9b2a05161be1c
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-11-26 19:32:48 +0530

    Added tier to deal investor

--- db/migrate/20221126134545_add_tier_to_deal_investor.rb
+++ db/migrate/20221126134545_add_tier_to_deal_investor.rb
@@ -1,5 +1,8 @@
 class AddTierToDealInvestor < ActiveRecord::Migration[7.0]
   def change
     add_column :deal_investors, :tier, :string, limit: 10
+    Entity.all.each do |e|
+      SetupFolders.call(entity: e)
+    end
   end
 end
