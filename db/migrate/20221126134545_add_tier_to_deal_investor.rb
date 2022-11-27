4966f72b1fa926c664b6b6faabd66bc256b574e3
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-11-27 10:47:35 +0530

    Fixed bug

--- db/migrate/20221126134545_add_tier_to_deal_investor.rb
+++ db/migrate/20221126134545_add_tier_to_deal_investor.rb
@@ -1,8 +1,5 @@
 class AddTierToDealInvestor < ActiveRecord::Migration[7.0]
   def change
     add_column :deal_investors, :tier, :string, limit: 10
-    Entity.all.each do |e|
-      SetupFolders.call(entity: e)
-    end
   end
 end
