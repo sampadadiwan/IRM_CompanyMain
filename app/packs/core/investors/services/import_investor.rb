91242215dadaedc9614913ee2bdce53ae31b8c8f
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-11-27 20:38:05 +0530

    Added ImportUtil to ImportInvestor

--- app/packs/core/investors/services/import_investor.rb
+++ app/packs/core/investors/services/import_investor.rb
@@ -1,18 +1,15 @@
-class ImportInvestor
+class ImportInvestor < ImportUtil
   include Interactor
 
   STANDARD_HEADERS = %w[Name Category Tags City Fund].freeze
 
-  def call
-    if context.import_upload.present? && context.import_file.present?
-      process_investor(context.import_file, context.import_upload)
-    else
-      context.fail!(message: "Required inputs not present")
-    end
+  def standard_headers
+    STANDARD_HEADERS
   end
 
   def save_investor(user_data, import_upload, custom_field_headers)
     # puts "processing #{user_data}"
+    saved = false
     investor = Investor.where(investor_name: user_data['Name'], entity_id: import_upload.entity_id).first
     if investor.present?
       Rails.logger.debug { "Investor with name #{user_data['Name']} already exists for entity #{import_upload.entity_id}" }
@@ -27,11 +24,12 @@ class ImportInvestor
       setup_custom_fields(user_data, investor, custom_field_headers)
 
       Rails.logger.debug { "Saving Investor with name '#{investor.investor_name}'" }
-      investor.save
+      saved = investor.save
 
     end
 
     add_to_fund(user_data, import_upload, investor)
+    saved
   end
 
   def add_to_fund(user_data, import_upload, investor)
@@ -55,37 +53,24 @@ class ImportInvestor
     end
   end
 
-  def setup_custom_fields(user_data, model, custom_field_headers)
-    # Were any custom fields passed in ? Set them up
-    if custom_field_headers.length.positive?
-      model.properties ||= {}
-      custom_field_headers.each do |cfh|
-        model.properties[cfh.parameterize.underscore] = user_data[cfh]
-      end
-    end
-  end
+  def process_row(headers, custom_field_headers, row, import_upload)
+    # create hash from headers and cells
 
-  def process_investor(_file, import_upload)
-    headers = context.headers
-    custom_field_headers = headers - STANDARD_HEADERS
-
-    data = context.data
-    # Parse the XL rows
-
-    data.each_with_index do |row, idx|
-      next if idx.zero? # skip header row
-
-      # create hash from headers and cells
-      user_data = [headers, row].transpose.to_h
-
+    user_data = [headers, row].transpose.to_h
+    begin
       if save_investor(user_data, import_upload, custom_field_headers)
         import_upload.processed_row_count += 1
+        row << "Success"
       else
         import_upload.failed_row_count += 1
+        row << "Error"
       end
-
-      # To indicate progress
-      import_upload.save if (idx % 10).zero?
+    rescue StandardError => e
+      Rails.logger.debug e.message
+      row << "Error #{e.message}"
+      Rails.logger.debug user_data
+      Rails.logger.debug row
+      import_upload.failed_row_count += 1
     end
   end
 end
