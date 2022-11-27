2356d7bc6eb1126d507bfa401165fd832489e4b6
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-11-27 10:28:54 +0530

    Fixed bug

--- db/migrate/20221126145706_add_ancestry_to_folder.rb
+++ db/migrate/20221126145706_add_ancestry_to_folder.rb
@@ -1,6 +1,7 @@
 class AddAncestryToFolder < ActiveRecord::Migration[7.0]
   def change
-    # add_column :folders, :ancestry, :string
-    # add_index :folders, :ancestry
+    add_column :folders, :ancestry, :string
+    add_index :folders, :ancestry
   end
+  Folder.build_ancestry_from_parent_ids!
 end
--- db/migrate/20221127040215_remove_parent_from_folder.rb
+++ db/migrate/20221127040215_remove_parent_from_folder.rb
@@ -1,6 +1,6 @@
 class RemoveParentFromFolder < ActiveRecord::Migration[7.0]
   def change
-    # remove_column :folders, :parent_id
-    # remove_column :folders, :path_ids    
+    remove_column :folders, :parent_id
+    remove_column :folders, :path_ids    
   end
 end
