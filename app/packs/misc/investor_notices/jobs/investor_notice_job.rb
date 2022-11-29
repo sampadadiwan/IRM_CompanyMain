53cd8203a04866108a114ce2e6b37f689e651b49
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-11-29 17:49:41 +0530

    Fixed bug with investor notice entry generation

--- app/packs/misc/investor_notices/jobs/investor_notice_job.rb
+++ app/packs/misc/investor_notices/jobs/investor_notice_job.rb
@@ -6,10 +6,10 @@ class InvestorNoticeJob < ApplicationJob
       if id.present?
         investor_notice = InvestorNotice.find(id)
 
-        investor_notice.access_rights.each do |ar|
-          ar.investors.each do |investor|
-            next if InvestorNoticeEntry.where(investor_id: investor.id, investor_notice_id: investor_notice.id).first.present?
-
+        investor_notice.entity.investors.each do |investor|
+          if InvestorNoticeEntry.where(investor_id: investor.id, investor_notice_id: investor_notice.id).first.present?
+            Rails.logger.debug { "InvestorNoticeJob: InvestorNoticeEntry already present for #{investor.investor_name} for notice #{investor_notice.id}" }
+          else
             InvestorNoticeEntry.create!(investor_id: investor.id, investor_notice_id: investor_notice.id,
                                         investor_entity_id: investor.investor_entity_id,
                                         entity_id: investor_notice.entity_id, active: true)
