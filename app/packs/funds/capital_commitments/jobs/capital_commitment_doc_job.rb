e0b42d9bd4e5c7efb9155aac1d975c400dc5e565
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-12-07 09:32:36 +0530

    Not deleting existing generated docs for commitments

--- app/packs/funds/capital_commitments/jobs/capital_commitment_doc_job.rb
+++ app/packs/funds/capital_commitments/jobs/capital_commitment_doc_job.rb
@@ -15,7 +15,7 @@ class CapitalCommitmentDocJob < ApplicationJob
         @templates.each do |fund_doc_template|
           Rails.logger.debug { "Generating #{fund_doc_template.name} for fund #{@fund.name}, for user #{kyc.user.full_name}" }
           # Delete any existing signed documents
-          @capital_commitment.documents.where(signed_by_id: kyc.user.id).each(&:destroy)
+          # @capital_commitment.documents.where(signed_by_id: kyc.user.id).each(&:destroy)
           # Generate a new signed document
           CapitalCommitmentDocGenerator.new(@capital_commitment, fund_doc_template, kyc.user)
         end
