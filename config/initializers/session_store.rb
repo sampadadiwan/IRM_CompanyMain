36b3ee677a3ba7b6ba4cfdfb43b2b457fdfda9d5
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-12-09 09:55:38 +0530

    Added cookie domain initializer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: 'caphive_session', domain: :all
+Rails.application.config.session_store :cookie_store, key: '_caphive_session', domain: :all, tld_length: 2
