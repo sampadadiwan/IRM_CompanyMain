af071b0c7e91fb0a6248720495ee243e243a60dd
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-12-10 10:04:41 +0530

    Added bulk import of remittances in capital call job

--- app/packs/funds/capital_calls/jobs/capital_call_job.rb
+++ app/packs/funds/capital_calls/jobs/capital_call_job.rb
@@ -1,9 +1,12 @@
 class CapitalCallJob < ApplicationJob
   # This job can generate multiple capital remittances, which cause deadlocks. Hence serial process these jobs
   queue_as :serial
+  attr_accessor :remittances
 
   # This is idempotent, we should be able to call it multiple times for the same CapitalCall
   def perform(capital_call_id, type = "Generate")
+    @remittances = []
+
     Chewy.strategy(:sidekiq) do
       case type
       when "Generate"
@@ -18,20 +21,37 @@ class CapitalCallJob < ApplicationJob
     @capital_call = CapitalCall.find(capital_call_id)
     @capital_call.fund.capital_commitments.each_with_index do |capital_commitment, _idx|
       # Check if we alread have a CapitalRemittance for this commitment
-      cr = CapitalRemittance.where(capital_call_id: @capital_call.id,
-                                   investor_id: capital_commitment.investor.id,
-                                   folio_id: capital_commitment.folio_id).first
-      if cr
-        logger.debug "CapitalCallJob: Skipping as CapitalRemittance exists for #{capital_commitment.investor.investor_name} for #{@capital_call.name}"
-      else
-        logger.debug "CapitalCallJob: Creating CapitalRemittance for #{capital_commitment.investor.investor_name} for #{@capital_call.name}"
-        # Note the due amount for the call is calculated automatically inside CapitalRemittance
-        CapitalRemittance.create(capital_call: @capital_call, fund: @capital_call.fund,
+      logger.debug "CapitalCallJob: Creating CapitalRemittance for #{capital_commitment.investor.investor_name} for #{@capital_call.name}"
+      # Note the due amount for the call is calculated automatically inside CapitalRemittance
+      cr = CapitalRemittance.new(capital_call: @capital_call, fund: @capital_call.fund,
                                  entity: @capital_call.entity, investor: capital_commitment.investor, capital_commitment:, folio_id: capital_commitment.folio_id,
                                  status: "Pending", verified: @capital_call.generate_remittances_verified)
 
-      end
+      next unless cr.valid?
+
+      cr.run_callbacks(:save) { false }
+      cr.run_callbacks(:create) { false }
+      @remittances << cr
     end
+
+    # import the rows
+    CapitalRemittance.import @remittances
+
+    ###########################################
+    # When the remittances are created - there is no collected amount, hence code below is not required
+    ###########################################
+
+    # @capital_call.reload
+    # # Ensure the counter caches are updated
+    # @capital_call.collected_amount_cents = @capital_call.capital_remittances.sum(:collected_amount_cents)
+    # @capital_call.save
+
+    # @capital_call.capital_remittances.each do |cr|
+    #   cr.capital_commitment.collected_amount_cents = cr.capital_commitment.capital_remittances.sum(:collected_amount_cents)
+    #   cr.capital_commitment.save
+    # end
+    # @capital_call.fund.collected_amount_cents = @capital_call.fund.capital_remittances.sum(:collected_amount_cents)
+    # @capital_call.fund.save
   end
 
   def notify(capital_call_id)
