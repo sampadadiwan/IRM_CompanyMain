ce41d39e53a71339511ccc8677f0b8195b5ba55a
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-12-12 14:34:00 +0530

    Added ElasticImporterJob to github actions

--- app/packs/misc/elastic/elastic_importer_job.rb
+++ app/packs/misc/elastic/elastic_importer_job.rb
@@ -26,26 +26,50 @@ class ElasticImporterJob < ApplicationJob
   end
 
   def reset
-    UserIndex.reset
-    EntityIndex.reset
-    InvestmentIndex.reset
-    AccessRightIndex.reset
-    DealInvestorIndex.reset
-    DocumentIndex.reset
-    HoldingIndex.reset
-    InvestorIndex.reset
-    InvestorAccessIndex.reset
-    NoteIndex.reset
-    SecondarySaleIndex.reset
-    HoldingAuditTrailIndex.reset
-    ExcerciseIndex.reset
-    OfferIndex.reset
-    TaskIndex.reset
-    InvestmentOpportunityIndex.reset
-    FundIndex.reset
-    CapitalCommitmentIndex.reset
-    CapitalRemittanceIndex.reset
-    CapitalDistributionPaymentIndex.import
-    InvestorKycIndex.reset
+    UserIndex.reset!
+    EntityIndex.reset!
+    InvestmentIndex.reset!
+    AccessRightIndex.reset!
+    DealInvestorIndex.reset!
+    DocumentIndex.reset!
+    HoldingIndex.reset!
+    InvestorIndex.reset!
+    InvestorAccessIndex.reset!
+    NoteIndex.reset!
+    SecondarySaleIndex.reset!
+    HoldingAuditTrailIndex.reset!
+    ExcerciseIndex.reset!
+    OfferIndex.reset!
+    TaskIndex.reset!
+    InvestmentOpportunityIndex.reset!
+    FundIndex.reset!
+    CapitalCommitmentIndex.reset!
+    CapitalRemittanceIndex.reset!
+    CapitalDistributionPaymentIndex.reset!
+    InvestorKycIndex.reset!
+  end
+
+  def create
+    UserIndex.create
+    EntityIndex.create
+    InvestmentIndex.create
+    AccessRightIndex.create
+    DealInvestorIndex.create
+    DocumentIndex.create
+    HoldingIndex.create
+    InvestorIndex.create
+    InvestorAccessIndex.create
+    NoteIndex.create
+    SecondarySaleIndex.create
+    HoldingAuditTrailIndex.create
+    ExcerciseIndex.create
+    OfferIndex.create
+    TaskIndex.create
+    InvestmentOpportunityIndex.create
+    FundIndex.create
+    CapitalCommitmentIndex.create
+    CapitalRemittanceIndex.create
+    CapitalDistributionPaymentIndex.create
+    InvestorKycIndex.create
   end
 end
