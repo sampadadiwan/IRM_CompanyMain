b222e066b890c9b400da7dd1cac72535a8a67bf1
Mohith Thimmaiah <thimmaiah@gmail.com>
2022-12-20 21:07:41 +0530

    Removed tags for documents

--- db/migrate/20221220152352_add_tag_list_to_document.rb
+++ db/migrate/20221220152352_add_tag_list_to_document.rb
@@ -1,6 +1,6 @@
 class AddTagListToDocument < ActiveRecord::Migration[7.0]
   def change
-    # add_column :documents, :tag_list, :string, limit: 60
+    add_column :documents, :tag_list, :string, limit: 60
   end
 
   Document.all.each do |doc|
