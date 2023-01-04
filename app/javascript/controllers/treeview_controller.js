9400537adb0ca15fa22981608ebfb40cb37074c4
Mohith Thimmaiah <thimmaiah@gmail.com>
2023-01-04 12:07:26 +0530

    Added jstree open folder logic

--- app/javascript/controllers/treeview_controller.js
+++ app/javascript/controllers/treeview_controller.js
@@ -8,24 +8,39 @@ export default class extends Controller {
             "core" : {
               "themes" : {
                 "variant" : "large"
-              }
+              },
+              expand_selected_onload: true,
             },
+            expand_selected_onload: true,
             "checkbox" : {
               "keep_selected_style" : false
             },
             "plugins" : [ "wholerow", "checkbox" ]
           });
           
-          $("#tree_view").jstree({ "plugins" : ["themes","html_data","ui"] });
+          $("#tree_view").jstree({ "plugins" : ["themes","html_data","ui"] }).on('ready.jstree', function (e, data) {
+            console.log("jstree initialized");
+
+            // Open the first level nodes
+            $("#tree_view").jstree("open_node", $("#tree_view li"));
+            let selected_folder_id = $("#selected_folder_id").val();
+            
+            // Open the selected folder
+            $("#tree_view").jstree('open_node', `#folder_${selected_folder_id}`, function(e,d) {
+              for (var i = 0; i < e.parents.length; i++) {
+                $("#tree_view").jstree('open_node', e.parents[i]);
+              };
+            });
+
+          });
+
+          // Show documents in the clicked folder
           $("#tree_view li").on("click", "a", 
               function() {
                   document.location.href = this;
               }
           );
-      
-          $("#tree_view").jstree("open_all")
-          
        
-          console.log("jstree initialized");
-    }
+          
+    }  
 }
