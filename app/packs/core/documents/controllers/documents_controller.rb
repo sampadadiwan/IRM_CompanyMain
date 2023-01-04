18000cf7311547ed83a46935b49aa4b9aeb84f03
Mohith Thimmaiah <thimmaiah@gmail.com>
2023-01-04 20:34:30 +0530

    Added turboframes to load docs in the tree view

--- app/packs/core/documents/controllers/documents_controller.rb
+++ app/packs/core/documents/controllers/documents_controller.rb
@@ -30,7 +30,7 @@ class DocumentsController < ApplicationController
       @documents = @documents.where(folder_id: folder_ids)
     end
     @documents = @documents.order(id: :desc)
-    @documents = @documents.includes(:folder).page params[:page]
+    @documents = @documents.includes(:folder).page(params[:page]).per(2)
   end
 
   def investor_documents
