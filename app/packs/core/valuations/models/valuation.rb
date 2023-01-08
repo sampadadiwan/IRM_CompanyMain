c667f5857d5dd4a81826e63f114abae06dc87a7e
Mohith Thimmaiah <thimmaiah@gmail.com>
2023-01-08 12:10:37 +0530

    Added formatting to valuations form

--- app/packs/core/valuations/models/valuation.rb
+++ app/packs/core/valuations/models/valuation.rb
@@ -1,17 +1,3 @@
-# == Schema Information
-#
-# Table name: valuations
-#
-#  id                        :integer          not null, primary key
-#  entity_id                 :integer          not null
-#  valuation_date            :date
-#  pre_money_valuation_cents :decimal(20, 2)   default("0.00")
-#  per_share_value_cents     :decimal(15, 2)   default("0.00")
-#  created_at                :datetime         not null
-#  updated_at                :datetime         not null
-#  form_type_id              :integer
-#
-
 class Valuation < ApplicationRecord
   belongs_to :entity
   belongs_to :owner, polymorphic: true, optional: true, touch: true
