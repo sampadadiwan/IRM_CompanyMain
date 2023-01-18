8b9d95aa79d294c2f494d3a985bbb5930be54848
Mohith Thimmaiah <thimmaiah@gmail.com>
2023-01-18 14:16:42 +0530

    Added fund ratios and calculations based on valuation date

--- app/packs/funds/funds/jobs/fund_calc_job.rb
+++ app/packs/funds/funds/jobs/fund_calc_job.rb
@@ -5,23 +5,36 @@ class FundCalcJob < ApplicationJob
   def perform(fund_id, user_id)
     Chewy.strategy(:sidekiq) do
       fund = Fund.find(fund_id)
-
       valuation = fund.valuations.order(valuation_date: :asc).last
-      calc = FundCalcs.new(fund, valuation)
 
-      # Blow off prev fund ratio calcs for this valuation
-      FundRatio.where(fund:, valuation:).delete_all
-
-      # Create the ratios
-      FundRatio.create(entity_id: fund.entity_id, fund:, valuation:, name: "Xirr", value: calc.compute_xirr)
-      FundRatio.create(entity_id: fund.entity_id, fund:, valuation:, name: "Moic", value: calc.compute_moic)
-      FundRatio.create(entity_id: fund.entity_id, fund:, valuation:, name: "Rvpi", value: calc.compute_rvpi)
-      FundRatio.create(entity_id: fund.entity_id, fund:, valuation:, name: "Dpi", value: calc.compute_dpi)
-      FundRatio.create(entity_id: fund.entity_id, fund:, valuation:, name: "Tvpi", value: calc.compute_tvpi)
+      calc_ratios(fund, valuation)
 
       # Notify the user
       notify(fund, user_id)
     end
+  end
+
+  def calc_ratios(fund, valuation)
+    # Blow off prev fund ratio calcs for this valuation
+    FundRatio.where(fund:, valuation:).delete_all
+
+    calc = FundCalcs.new(fund, valuation)
+    # Create the ratios
+    xirr = calc.compute_xirr
+    FundRatio.create(entity_id: fund.entity_id, fund:, valuation:, name: "Xirr", value: xirr, display_value: "#{xirr} %")
+    FundRatio.create(entity_id: fund.entity_id, fund:, valuation:, name: "Moic", value: calc.compute_moic, display_value: calc.compute_moic.to_s)
+
+    value = calc.compute_rvpi
+    display_value = value ? "#{value}x" : nil
+    FundRatio.create(entity_id: fund.entity_id, fund:, valuation:, name: "Rvpi", value:, display_value:)
+
+    value = calc.compute_dpi
+    display_value = value ? "#{value}x" : nil
+    FundRatio.create(entity_id: fund.entity_id, fund:, valuation:, name: "Dpi", value:, display_value:)
+
+    value = calc.compute_tvpi
+    display_value = value ? "#{value}x" : nil
+    FundRatio.create(entity_id: fund.entity_id, fund:, valuation:, name: "Tvpi", value:, display_value:)
   end
 
   def notify(fund, user_id)
--- app/packs/funds/funds/models/fund_calcs.rb
+++ app/packs/funds/funds/models/fund_calcs.rb
@@ -9,7 +9,7 @@ class FundCalcs
   end
 
   def compute_rvpi
-    @rvpi = (@valuation.valuation_cents / @collected_amount_cents).round(2) if @valuation
+    @rvpi = (@valuation.valuation_cents / @collected_amount_cents).round(2) if @collected_amount_cents.positive?
   end
 
   def compute_dpi
@@ -25,24 +25,20 @@ class FundCalcs
   end
 
   def compute_xirr
-    if @valuation
+    cf = Xirr::Cashflow.new
 
-      cf = Xirr::Cashflow.new
-
-      @fund.capital_remittance_payments.where("created_at <= ?", @valuation.valuation_date).each do |cr|
-        cf << Xirr::Transaction.new(-1 * cr.amount_cents, date: cr.payment_date)
-      end
-
-      @fund.capital_distribution_payments.where("created_at <= ?", @valuation.valuation_date).each do |cdp|
-        cf << Xirr::Transaction.new(cdp.amount_cents, date: cdp.payment_date)
-      end
-
-      cf << Xirr::Transaction.new(@valuation.valuation_cents, date: @valuation.valuation_date)
-
-      Rails.logger.debug { "fund.xirr cf: #{cf}" }
-      Rails.logger.debug { "fund.xirr irr: #{cf.xirr}" }
-      (cf.xirr * 100).round(2)
-
+    @fund.capital_remittance_payments.where("created_at <= ?", @valuation.valuation_date).each do |cr|
+      cf << Xirr::Transaction.new(-1 * cr.amount_cents, date: cr.payment_date)
     end
+
+    @fund.capital_distribution_payments.where("created_at <= ?", @valuation.valuation_date).each do |cdp|
+      cf << Xirr::Transaction.new(cdp.amount_cents, date: cdp.payment_date)
+    end
+
+    cf << Xirr::Transaction.new(@valuation.valuation_cents, date: @valuation.valuation_date)
+
+    Rails.logger.debug { "fund.xirr cf: #{cf}" }
+    Rails.logger.debug { "fund.xirr irr: #{cf.xirr}" }
+    (cf.xirr * 100).round(2)
   end
 end
