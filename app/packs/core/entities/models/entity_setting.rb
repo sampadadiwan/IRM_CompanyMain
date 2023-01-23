a841e9ea3979a7d9fc5e89a6908a18bd9cdb847f
Mohith Thimmaiah <thimmaiah@gmail.com>
2023-01-23 11:15:29 +0530

    Better comments

--- app/packs/core/entities/models/entity_setting.rb
+++ app/packs/core/entities/models/entity_setting.rb
@@ -1,3 +1,6 @@
+# Stores all settings not used by the UI / used typically by background jobs to handle entity related features
+# This enables us to make the Entity small as its used in every request,
+# while moving other attributes to EntitySetting
 class EntitySetting < ApplicationRecord
   belongs_to :entity
 end
