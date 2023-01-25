2877bfb5741b5712015551122da2ab87a37dc17c
Mohith Thimmaiah <thimmaiah@gmail.com>
2023-01-25 18:13:14 +0530

    Removed funding_round from fund and IO

--- db/migrate/20230125122202_change_funding_round_for_funds.rb
+++ db/migrate/20230125122202_change_funding_round_for_funds.rb
@@ -2,5 +2,9 @@ class ChangeFundingRoundForFunds < ActiveRecord::Migration[7.0]
   def change
     change_column :funds, :funding_round_id, :bigint, null: true
     change_column :investment_opportunities, :funding_round_id, :bigint, null: true 
+
+    Entity.where(entity_type: "Investment Fund").each do |e|
+      e.funding_rounds.each(&:destroy)
+    end
   end
 end
