469bdda95126299892e9dee2c97f13a0ca2e2cd2
Mohith Thimmaiah <thimmaiah@gmail.com>
2023-01-25 20:27:04 +0530

    Added data room to funds

--- app/packs/core/folders/models/concerns/with_data_room.rb
+++ app/packs/core/folders/models/concerns/with_data_room.rb
@@ -7,7 +7,7 @@ module WithDataRoom
   end
 
   def create_data_room
-    self.data_room_folder = document_folder.children.where(entity_id:, name: data_room_name, folder_type: :regular, owner: self).first_or_create
+    self.data_room_folder ||= document_folder.children.where(entity_id:, name: data_room_name, folder_type: :regular, owner: self).first_or_create
     save
   end
 
