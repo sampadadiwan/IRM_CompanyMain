b119db568c33c806bd995a67bb13df8ce3cdf699
Mohith Thimmaiah <thimmaiah@gmail.com>
2023-02-02 10:02:59 +0530

    Added units for investors details

--- app/packs/core/access_rights/mailers/access_rights_mailer.rb
+++ app/packs/core/access_rights/mailers/access_rights_mailer.rb
@@ -4,22 +4,27 @@ class AccessRightsMailer < ApplicationMailer
   def notify_access
     # We need to figure out all the users impacted by this access right
     @access_right = AccessRight.includes(:owner, :investor).find params[:access_right_id]
-    # Only send to confirmed users
-    confirmed_emails = User.where(email: @access_right.investor_emails).where.not(confirmed_at: nil).collect(&:email).join(",")
 
-    if confirmed_emails.present?
-      if @access_right.access_to_category.present?
-        bcc = sandbox_email(@access_right, confirmed_emails)
-        to = ENV.fetch('SUPPORT_EMAIL', nil)
-      else
-        to = sandbox_email(@access_right, confirmed_emails)
-        bcc = nil
+    if @access_right
+      # Only send to confirmed users
+      confirmed_emails = User.where(email: @access_right.investor_emails).where.not(confirmed_at: nil).collect(&:email).join(",")
+
+      if confirmed_emails.present?
+        if @access_right.access_to_category.present?
+          bcc = sandbox_email(@access_right, confirmed_emails)
+          to = ENV.fetch('SUPPORT_EMAIL', nil)
+        else
+          to = sandbox_email(@access_right, confirmed_emails)
+          bcc = nil
+        end
+
+        mail(from: from_email(@access_right.entity),
+             to:,
+             bcc:,
+             subject: "Access Granted to #{@access_right.owner_type} #{@access_right.owner.name} by #{@access_right.entity.name}")
       end
-
-      mail(from: from_email(@access_right.entity),
-           to:,
-           bcc:,
-           subject: "Access Granted to #{@access_right.owner_type} #{@access_right.owner.name} by #{@access_right.entity.name}")
+    else
+      Rails.logger.debug { "Could not find access right with id #{params[:access_right_id]}" }
     end
   end
 end
