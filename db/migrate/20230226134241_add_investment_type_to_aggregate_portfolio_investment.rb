65a8de69a09c848f39b7142c0bb94e087cc33060
Mohith Thimmaiah <thimmaiah@gmail.com>
2023-02-26 19:55:41 +0530

    Added investment type in aggregate investment

--- db/migrate/20230226134241_add_investment_type_to_aggregate_portfolio_investment.rb
+++ db/migrate/20230226134241_add_investment_type_to_aggregate_portfolio_investment.rb
@@ -2,5 +2,10 @@ class AddInvestmentTypeToAggregatePortfolioInvestment < ActiveRecord::Migration[
   def change
     add_column :aggregate_portfolio_investments, :investment_type, :string, limit: 20
     change_column :portfolio_investments, :investment_type, :string, limit: 20
+
+    PortfolioInvestment.all.each do |pi|
+      pi.aggregate_portfolio_investment = nil
+      pi.save
+    end
   end
 end
