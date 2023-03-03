085981c6d0c1ea82377930365d09ca0fe28fe981
Mohith Thimmaiah <thimmaiah@gmail.com>
2023-03-03 15:14:22 +0530

    Added basic agg portfolio charts

--- app/packs/funds/funds/helpers/portfolio_helper.rb
+++ app/packs/funds/funds/helpers/portfolio_helper.rb
@@ -1,4 +1,24 @@
 module PortfolioHelper
+  def investment_fmv_by_company(fund)
+    aggregate_portfolio_investments = fund.aggregate_portfolio_investments
+    grouped = aggregate_portfolio_investments
+              .map { |k| [k.portfolio_company_name, k.fmv.to_d] }
+
+    pie_chart_with_options grouped
+  end
+
+  def bought_sold_fmv(fund)
+    aggregate_portfolio_investments = fund.aggregate_portfolio_investments
+    grouped = aggregate_portfolio_investments
+              .map { |k| [k.portfolio_company_name, k.fmv.to_d, k.bought_amount.to_d, k.sold_amount.to_d] }
+
+    column_chart [
+      { name: "FMV", data: grouped.map { |k| [k[0], k[1]] } },
+      { name: "Bought", data: grouped.map { |k| [k[0], k[2]] } },
+      { name: "Sold", data: grouped.map { |k| [k[0], k[3]] } }
+    ]
+  end
+
   def share_price_line(portfolio_company)
     valuations = portfolio_company.valuations
     valuations = valuations.group_by { |v| v.valuation_date.strftime("%m/%Y") }
