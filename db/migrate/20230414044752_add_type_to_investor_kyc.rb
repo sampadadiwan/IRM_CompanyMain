cc7824803e497381100150cb04e8a239a91f6967
thimmaiah <thimmaiah@gmail.com>
2023-04-14 10:43:24 +0530

    Added kyc type field to kyc

--- db/migrate/20230414044752_add_type_to_investor_kyc.rb
+++ db/migrate/20230414044752_add_type_to_investor_kyc.rb
@@ -1,6 +1,6 @@
 class AddTypeToInvestorKyc < ActiveRecord::Migration[7.0]
   def change
     add_column :investor_kycs, :kyc_type, :string, limit: 15, default: "Individual"
-    InvestorKyc.update_all(kyc_type: "Individual")
+    InvestorKyc.update_all(kyc_type: "individual")
   end
 end
