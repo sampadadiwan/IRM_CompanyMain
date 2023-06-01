234e3eee695493bfe3e53922cef7191f3b352d34
thimmaiah <thimmaiah@gmail.com>
2023-06-01 21:48:40 +0530

    Made fee fields non mandatory

--- app/packs/core/fees/models/fee.rb
+++ app/packs/core/fees/models/fee.rb
@@ -2,7 +2,7 @@ class Fee < ApplicationRecord
   belongs_to :owner, polymorphic: true, touch: true
   belongs_to :entity
 
-  validates :amount, :amount_label, :bank_account_number, :ifsc_code, presence: true
+  validates :amount, :amount_label, :advisor_name, presence: true
 
   monetize :amount_cents,
            with_currency: ->(f) { f.owner.respond_to?(:currency) ? f.owner.currency : f.entity.currency }
