98501c4ca73d96f949a86a521e893e66f90f7885
thimmaiah <thimmaiah@gmail.com>
2023-06-18 14:47:43 +0530

    Added kpi_lines_by_date charts

--- app/packs/startups/kpis/helpers/kpis_helper.rb
+++ app/packs/startups/kpis/helpers/kpis_helper.rb
@@ -1,2 +1,9 @@
 module KpisHelper
+  def kpi_lines_by_date(kpi_reports)
+    kpis = Kpi.joins(:kpi_report).where(kpi_report_id: kpi_reports.pluck(:id).uniq).order("kpi_reports.as_of asc").group_by(&:name)
+
+    chart_data = kpis.map { |name, arr| [name:, data: arr.map { |kpi| [kpi.kpi_report.as_of, kpi.value] }] }.flatten
+
+    line_chart chart_data
+  end
 end
