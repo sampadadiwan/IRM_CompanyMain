71fa826c1ed94d164b6625a987c07f9632caf51e
thimmaiah <thimmaiah@gmail.com>
2023-06-27 16:54:39 +0530

    Added no_hover_table style for nested tables

--- app/javascript/controllers/commitments_controller.js
+++ app/javascript/controllers/commitments_controller.js
@@ -3,38 +3,43 @@ import  ServerDatatableController from "controllers/server_datatable_controller"
 
 export default class extends ServerDatatableController {
  
+  default = [
+    {"data": "commitment_type"},
+    {"data": "folio_id"},
+    {"data": "investor_name"},
+    {"data": "full_name"},
+    {"data": "unit_type"},
+    {"data": "committed_amount"},
+    {"data": "percentage"},
+    {"data": "call_amount"},
+    {"data": "collected_amount"},
+    {"data": "pending_amount"},          
+    {"data": "distribution_amount"},          
+    {"data": "dt_actions"}
+  ];
+
+  with_docs = [
+    {"data": "commitment_type"},
+    {"data": "folio_id"},
+    {"data": "investor_name"},
+    {"data": "full_name"},
+    {"data": "unit_type"},
+    {"data": "committed_amount"},
+    {"data": "percentage"},
+    {"data": "call_amount"},
+    {"data": "collected_amount"},  
+    {"data": "pending_amount"},          
+    {"data": "distribution_amount"},          
+    {"data": "document_names"},
+    {"data": "dt_actions"}
+  ];
+
+
   columns() {
     if ($("#show_docs").length > 0) {
-      return [
-        {"data": "commitment_type"},
-        {"data": "folio_id"},
-        {"data": "investor_name"},
-        {"data": "full_name"},
-        {"data": "unit_type"},
-        {"data": "committed_amount"},
-        {"data": "percentage"},
-        {"data": "call_amount"},
-        {"data": "collected_amount"},  
-        {"data": "pending_amount"},          
-        {"data": "distribution_amount"},          
-        {"data": "document_names"},
-        {"data": "dt_actions"}
-      ]
+      return this.with_docs;
     } else {
-      return [
-        {"data": "commitment_type"},
-        {"data": "folio_id"},
-        {"data": "investor_name"},
-        {"data": "full_name"},
-        {"data": "unit_type"},
-        {"data": "committed_amount"},
-        {"data": "percentage"},
-        {"data": "call_amount"},
-        {"data": "collected_amount"},
-        {"data": "pending_amount"},          
-        {"data": "distribution_amount"},          
-        {"data": "dt_actions"}
-      ]
+      return this.default;
     }
   }
 }
