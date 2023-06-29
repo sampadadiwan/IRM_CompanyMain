6aa267e9a9358d8199f91ede0553d5a230d5b60a
thimmaiah <thimmaiah@gmail.com>
2023-06-29 10:44:56 +0530

    Added filter investor by owner

--- app/packs/core/investors/controllers/investors_controller.rb
+++ app/packs/core/investors/controllers/investors_controller.rb
@@ -4,10 +4,18 @@ class InvestorsController < ApplicationController
 
   # GET /investors or /investors.json
   def index
-    @investors = policy_scope(Investor).joins(:entity)
+    @investors = policy_scope(Investor)
     authorize(Investor)
 
     @investors = @investors.where(category: params[:category]) if params[:category]
+    if params[:owner_id].present? && params[:owner_type].present?
+      owner = params[:owner_type].constantize.find(params[:owner_id])
+      authorize(owner, :show?)
+      @investors = owner.investors
+    elsif !current_user.has_cached_role?(:company_admin)
+      # No owner, he must be company admin or employee with investor access, else show nothing
+      @investors = Investor.none
+    end
 
     if params[:search] && params[:search][:value].present?
       # This is only when the datatable sends a search query
@@ -20,6 +28,7 @@ class InvestorsController < ApplicationController
       @investors = Investor.where(id: ids)
     end
 
+    @investors = @investors.joins(:entity)
     @investors = @investors.page(params[:page]) if params[:all].blank?
     respond_to do |format|
       format.html
