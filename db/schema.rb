cd0a275e3b5087bd9f3a2c2fb48339664ccb5cd4
thimmaiah <thimmaiah@gmail.com>
2023-07-08 13:16:16 +0530

    Post merge of esignatures

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_08_065848) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_08_073247) do
   create_table "abraham_histories", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "controller_name"
     t.string "action_name"
@@ -747,6 +747,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_08_065848) do
     t.boolean "template", default: false
     t.boolean "send_email"
     t.boolean "sent_for_esign", default: false
+    t.string "provider_doc_id"
+    t.string "esign_status", limit: 20
+    t.string "display_on_page", limit: 6
     t.index ["deleted_at"], name: "index_documents_on_deleted_at"
     t.index ["entity_id"], name: "index_documents_on_entity_id"
     t.index ["folder_id"], name: "index_documents_on_folder_id"
@@ -768,6 +771,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_08_065848) do
     t.string "status", limit: 10
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "api_updates"
     t.index ["entity_id"], name: "index_e_signatures_on_entity_id"
     t.index ["owner_type", "owner_id"], name: "index_e_signatures_on_owner"
     t.index ["user_id"], name: "index_e_signatures_on_user_id"
