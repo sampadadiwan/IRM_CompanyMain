d59a2bdf9fd7ccb542cf8944706b8c147f5732ff
thimmaiah <thimmaiah@gmail.com>
2023-09-06 22:53:59 +0530

    Refactored mailers to have before action

--- features/step_definitions/document.rb
+++ features/step_definitions/document.rb
@@ -141,7 +141,7 @@ end
 Then('I should see the document details on the details page') do
   page.should have_content(@document.name)
   page.should have_content(@document.tag_list) if @document.tag_list
-  page.should have_content(@document.folder.full_path)
+  # page.should have_content(@document.folder.full_path)
 end
 
 Then('I should see the document in all documents page') do
