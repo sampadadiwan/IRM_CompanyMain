e4012c74072f7f8f8fd7057825fc49c24c787ed1
thimmaiah <thimmaiah@gmail.com>
2023-09-07 10:48:49 +0530

    Merged PR

--- app/packs/core/investor_kycs/mailers/investor_kyc_mailer.rb
+++ app/packs/core/investor_kycs/mailers/investor_kyc_mailer.rb
@@ -36,34 +36,4 @@ class InvestorKycMailer < ApplicationMailer
       send_mail(subject: subj)
     end
   end
-
-  def notify_kyc_required
-    @investor_kyc = InvestorKyc.find(params[:investor_kyc_id])
-    @user = User.find(params[:user_id])
-    email = sandbox_email(@investor_kyc, @user.email)
-    cc = @investor_kyc.entity.entity_setting.cc
-
-    if email.present?
-      subj = "Request to add KYC: #{@investor_kyc.entity.name}"
-      mail(from: from_email(@investor_kyc.entity),
-           to: email,
-           cc:,
-           subject: subj)
-    end
-  end
-
-  def kyc_required_reminder
-    @investor_kyc = InvestorKyc.find(params[:investor_kyc_id])
-    @user = User.find(params[:user_id])
-    email = sandbox_email(@investor_kyc, @user.email)
-    cc = @investor_kyc.entity.entity_setting.cc
-
-    if email.present?
-      subj = "Reminder to update KYC: #{@investor_kyc.entity.name}"
-      mail(from: from_email(@investor_kyc.entity),
-           to: email,
-           cc:,
-           subject: subj)
-    end
-  end
 end
