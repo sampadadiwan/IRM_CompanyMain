f92613cb5eb0fe2d2b53b69603cd7b2697002362
thimmaiah <thimmaiah@gmail.com>
2023-10-06 20:48:47 +0530

    Added kyc_docs to import upload dropdown

--- app/packs/misc/import_uploads/models/import_upload.rb
+++ app/packs/misc/import_uploads/models/import_upload.rb
@@ -23,7 +23,7 @@ class ImportUpload < ApplicationRecord
               "ACCOUNT_ENTRY_SAMPLE" => "/sample_uploads/account_entries.xlsx",
               "KPIS_SAMPLE" => "/sample_uploads/kpis.xlsx" }.freeze
 
-  TYPES = %w[InvestorAccess Investor CapitalCommittment CapitalCall CapitalRemittance CapitalRemittancePayment CapitalDistribution CapitalDistributionPayment Documents PortfolioInvestment Valuation FundDocs FundUnitSetting AccountEntry InvestorKyc InvestorAdvisor Holding Offer OfferDocs OptionsCustomData Kpi].sort.freeze
+  TYPES = %w[InvestorAccess Investor CapitalCommittment CapitalCall CapitalRemittance CapitalRemittancePayment CapitalDistribution CapitalDistributionPayment Documents PortfolioInvestment Valuation FundDocs FundUnitSetting AccountEntry InvestorKyc InvestorAdvisor Holding Offer OfferDocs OptionsCustomData Kpi KycDocs].sort.freeze
 
   belongs_to :entity
   belongs_to :owner, polymorphic: true
