67a7fb419c368ee28e66680f8ba7c08c743982dd
thimmaiah <thimmaiah@gmail.com>
2023-10-21 15:41:37 +0530

    Fixed bug in KPI names

--- app/packs/startups/kpis/models/kpi.rb
+++ app/packs/startups/kpis/models/kpi.rb
@@ -8,10 +8,11 @@ class Kpi < ApplicationRecord
   validates :display_value, length: { maximum: 30 }
 
   def custom_form_field
-    field = kpi_report.form_type.form_custom_fields.where(name: name.downcase).first
+    sanitized_name = name.downcase.strip
+    field = kpi_report.form_type.form_custom_fields.where(name: sanitized_name).first
     if field.nil?
-      kpi_report.form_type.form_custom_fields.where("meta_data like '%#{name.downcase}%'").find_each do |fcf|
-        field = fcf if fcf.meta_data.downcase.split(",").include?(name.downcase)
+      kpi_report.form_type.form_custom_fields.where("meta_data like '%#{sanitized_name}%'").find_each do |fcf|
+        field = fcf if fcf.meta_data.downcase.split(",").include?(sanitized_name)
       end
     end
     field ||= FormCustomField.new(field_type: "Unknown", name:)
