e5bf6dbd61c651ad6bd4c4c8f5bb39316e91b646
thimmaiah <thimmaiah@gmail.com>
2023-11-04 08:59:23 +0530

    Added better notifications for doc jobs

--- app/packs/funds/capital_commitments/jobs/capital_commitment_soa_job.rb
+++ app/packs/funds/capital_commitments/jobs/capital_commitment_soa_job.rb
@@ -7,6 +7,7 @@ class CapitalCommitmentSoaJob < ApplicationJob
       @capital_commitment = CapitalCommitment.find(capital_commitment_id)
       @fund = @capital_commitment.fund
       @investor = @capital_commitment.investor
+      @investor_kyc = @capital_commitment.investor_kyc
 
       # Try and get the template from the capital_commitment
       @templates = @capital_commitment.templates("SOA Template", template_name)
@@ -14,9 +15,12 @@ class CapitalCommitmentSoaJob < ApplicationJob
       Rails.logger.debug { "Generating documents for #{@investor.investor_name}, for fund #{@fund.name}" }
 
       @templates.each do |fund_doc_template|
-        Rails.logger.debug { "Generating #{fund_doc_template.name} for fund #{@fund.name}, for user #{@capital_commitment.investor_kyc&.full_name}" }
+        Rails.logger.debug { "Generating #{fund_doc_template.name} for fund #{@fund.name}, for user #{@investor_kyc&.full_name}" }
+        send_notification("Generating #{fund_doc_template.name} for fund #{@fund.name}, for user #{@investor_kyc&.full_name}", :info)
         # Generate a new signed document
         SoaGenerator.new(@capital_commitment, fund_doc_template, start_date, end_date, user_id)
+      rescue StandardError
+        send_notification("Error generating #{fund_doc_template.name} for fund #{@fund_name}, for user #{@investor_kyc&.full_name}")
       end
     end
 
