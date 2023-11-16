495f318e032c34c0faf2df535bed99701249b1e5
thimmaiah <thimmaiah@gmail.com>
2023-11-16 16:48:20 +0530

    Added pills

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,5 +1,5 @@
 Chartkick.options = {
-  colors: ["#4e73df", "#65C18C", "#9ADCFF", "#364F6B", "#3FC1C9", "#54BAB9", "#FC5185", "#FFBCBC", "#EDF6E5", "#FF964F"],
+  colors: ["#5d87ff", "#65C18C", "#9ADCFF", "#364F6B", "#3FC1C9", "#54BAB9", "#FC5185", "#FFBCBC", "#EDF6E5", "#FF964F"],
   # colors: ["#00008B", "#ff970f", "#0059FF", "#00008B", "#616669", "#B6B6B4", "#C19A6B", "#AF5500", "#AF5500", "#43302E"],
   download: true,
   adapter: "highcharts"
