787aa5552600ef747bcc09884ac4b0b57b46d03d
thimmaiah <thimmaiah@gmail.com>
2023-12-11 10:40:07 +0530

    Add cc only if entity is not in sandbox

--- app/packs/core/base/mailers/application_mailer.rb
+++ app/packs/core/base/mailers/application_mailer.rb
@@ -12,14 +12,16 @@ class ApplicationMailer < ActionMailer::Base
     if @entity.present?
       # Ensure we pick te right from address
       @from = from_email(@entity)
-
-      @cc = @entity.entity_setting.cc
-      # Sometimes we have an ovveride for the cc field in the investor access
-      investor_cc = @entity.investor_accesses.where(email: @user.email).first&.cc
-      if (@cc.nil? || @cc.blank?) && investor_cc.present?
-        @cc = investor_cc
-      elsif @cc.present? && investor_cc.present?
-        @cc += ",#{investor_cc}"
+      
+      unless @entity.sandox
+        @cc = @entity.entity_setting.cc
+        # Sometimes we have an ovveride for the cc field in the investor access
+        investor_cc = @entity.investor_accesses.where(email: @user.email).first&.cc
+        if (@cc.nil? || @cc.blank?) && investor_cc.present?
+          @cc = investor_cc
+        elsif @cc.present? && investor_cc.present?
+          @cc += ",#{investor_cc}"
+        end
       end
 
       @reply_to = @entity.entity_setting.reply_to.presence || @cc.presence || @from
