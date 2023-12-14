eef21fc2d8ae281d94de39c493a54df7efca74ff
thimmaiah <thimmaiah@gmail.com>
2023-12-14 11:09:54 +0530

    Added doc gen based on template name

--- app/packs/funds/funds/jobs/fund_capital_commitment_doc_job.rb
+++ app/packs/funds/funds/jobs/fund_capital_commitment_doc_job.rb
@@ -4,12 +4,16 @@ class FundCapitalCommitmentDocJob < ApplicationJob
   # user_id - The id of the user who is requesting the docs generation
   # fund_id - The id of the fund for which we want to generate docs for all capital_commitments.
   def perform(fund_id, user_id = nil, template_name: nil)
+    fund = Fund.find(fund_id)
+
     Chewy.strategy(:sidekiq) do
       # Need to generate docs for all commitments of the fund
-      fund = Fund.find(fund_id)
       fund.capital_commitments.each do |capital_commitment|
-        CapitalCommitmentDocJob.perform_later(capital_commitment.id, user_id, template_name:)
-      end
+        CapitalCommitmentDocJob.perform_now(capital_commitment.id, user_id, template_name:)
+      end      
     end
+
+    msg = "Document generation completed for #{fund.name}"
+    send_notification(msg, user_id, :success)
   end
 end
