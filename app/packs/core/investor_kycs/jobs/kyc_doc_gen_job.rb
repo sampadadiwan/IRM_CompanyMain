02a71804f28ded500a69ece428c9e3de7f700aaf
thimmaiah <thimmaiah@gmail.com>
2024-01-10 22:47:48 +0530

    Send error emails if kyc doc gen has errors

--- app/packs/core/investor_kycs/jobs/kyc_doc_gen_job.rb
+++ app/packs/core/investor_kycs/jobs/kyc_doc_gen_job.rb
@@ -4,6 +4,8 @@ class KycDocGenJob < ApplicationJob
   def perform(investor_kyc_id, document_template_ids, start_date, end_date,
               user_id: nil, entity_id: nil)
 
+    error_msg = []
+
     Chewy.strategy(:sidekiq) do
       investor_kycs = InvestorKyc.where(id: investor_kyc_id) if investor_kyc_id.present?
       # # If we have a fund then lets get the kycs for that fund
@@ -18,12 +20,18 @@ class KycDocGenJob < ApplicationJob
           send_notification("Generating #{document_template.name} for #{investor_kyc.full_name}", user_id)
           KycDocGenerator.new(investor_kyc, document_template, start_date, end_date, user_id)
         rescue Exception => e
-          send_notification("Error generating #{document_template.name} for #{investor_kyc.full_name} #{e.message}", user_id, "danger")
-          raise e
+          msg = "Error generating #{document_template.name} for #{investor_kyc.full_name} #{e.message}"
+          send_notification(msg, user_id, "danger")
+          # raise e
+          error_msg << { msg:, template: document_template&.name, folio_id: investor_kyc.capital_commitment.folio_id, investor_name: investor_kyc.full_name }
         end
       end
     end
 
+    if error_msg.present?
+      send_notification("Documentation generation completed with errors. Errors will be sent via email", user_id, :danger)
+      EntityMailer.with(entity_id: investor_kycs.last.entity_id, user_id:, error_msg:).doc_gen_errors.deliver_now
+    end
     send_notification("Invalid Dates", user_id, "danger") if start_date > end_date
     send_notification("Invalid Document Template", user_id, "danger") if document_template_ids.blank?
   end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "15.207.92.13", user: "ubuntu", roles: %w[app db web]
+server "dev.altconnects.com", user: "ubuntu", roles: %w[app db web]
 set :rails_env, "staging"
 set :stage, :staging
 
