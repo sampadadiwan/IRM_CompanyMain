2ab6eacf7db3c22766bfcddb5c44337018276254
shrikantgour <48914363+shrikantgour@users.noreply.github.com>
2024-01-19 19:30:21 +0530

    fixed template decorator format_nd (#187)

--- app/packs/core/base/decorators/template_decorator.rb
+++ app/packs/core/base/decorators/template_decorator.rb
@@ -25,13 +25,13 @@ class TemplateDecorator < ApplicationDecorator
       attr_name = method_name.to_s.gsub("date_format_", "")
       return send(attr_name)&.strftime("%d %B %Y")
 
-    elsif method_name.to_s.include?("format_")
-      attr_name = method_name.to_s.gsub("format_", "")
-      return h.number_with_precision(send(attr_name).to_d, precision: 2, delimiter: ",")
-
     elsif method_name.to_s.include?("format_nd_")
       attr_name = method_name.to_s.gsub("format_nd_", "")
       return h.number_with_precision(send(attr_name).to_d, precision: 0, delimiter: ",")
+
+    elsif method_name.to_s.include?("format_")
+      attr_name = method_name.to_s.gsub("format_", "")
+      return h.number_with_precision(send(attr_name).to_d, precision: 2, delimiter: ",")
 
     elsif method_name.to_s.include?("rupees_")
       attr_name = method_name.to_s.gsub("rupees_", "")
