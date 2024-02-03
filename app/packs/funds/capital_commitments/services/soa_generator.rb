58710cbc6e05fd4e3bd17ba09037cc04a8692ab9
shrikantgour <48914363+shrikantgour@users.noreply.github.com>
2024-02-03 12:18:37 +0530

    fixed soa generator (#196)

--- app/packs/funds/capital_commitments/services/soa_generator.rb
+++ app/packs/funds/capital_commitments/services/soa_generator.rb
@@ -41,7 +41,7 @@ class SoaGenerator
       capital_commitment: TemplateDecorator.decorate(capital_commitment),
       entity: capital_commitment.entity,
       fund: TemplateDecorator.decorate(capital_commitment.fund),
-      fund_units: TemplateDecorator.new(fund_units(capital_commitment, start_date, end_date)),
+      fund_units: TemplateDecorator.decorate(fund_units(capital_commitment, start_date, end_date)),
 
       capital_remittances: TemplateDecorator.decorate_collection(capital_commitment.capital_remittances.order(:remittance_date)),
       capital_remittances_between_dates: TemplateDecorator.decorate_collection(capital_commitment.capital_remittances.where(remittance_date: start_date..).where(remittance_date: ..end_date).order(:remittance_date)),
@@ -59,9 +59,9 @@ class SoaGenerator
       distribution_amounts_between_dates: TemplateDecorator.decorate(distribution_amounts(capital_commitment.capital_distribution_payments.where(payment_date: start_date..).where(payment_date: ..end_date), capital_commitment.fund.currency)),
       distribution_amounts_before_end_date: TemplateDecorator.decorate(distribution_amounts(capital_commitment.capital_distribution_payments.where(payment_date: ..end_date), capital_commitment.fund.currency)),
 
-      account_entries: TemplateDecorator.new(capital_commitment.account_entries),
-      account_entries_between_dates: TemplateDecorator.new(capital_commitment.account_entries.where(reporting_date: start_date..).where(reporting_date: ..end_date)),
-      account_entries_before_end_date: TemplateDecorator.new(capital_commitment.account_entries.where(reporting_date: ..end_date)),
+      account_entries: TemplateDecorator.decorate_collection(capital_commitment.account_entries),
+      account_entries_between_dates: TemplateDecorator.decorate_collection(capital_commitment.account_entries.where(reporting_date: start_date..).where(reporting_date: ..end_date)),
+      account_entries_before_end_date: TemplateDecorator.decorate_collection(capital_commitment.account_entries.where(reporting_date: ..end_date)),
 
       fund_ratios: TemplateDecorator.decorate_collection(capital_commitment.fund_ratios),
       fund_ratios_between_dates: TemplateDecorator.decorate_collection(capital_commitment.fund_ratios.where(end_date: start_date..).where(end_date: ..end_date)),
