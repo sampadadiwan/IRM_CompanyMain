ffdc9baab7ea3a65638b29710f4995a871f17fbe
thimmaiah <thimmaiah@gmail.com>
2024-02-03 22:02:51 +0530

    Refactored to use base class

--- app/packs/funds/capital_remittances/jobs/capital_remittances_bulk_action_job.rb
+++ app/packs/funds/capital_remittances/jobs/capital_remittances_bulk_action_job.rb
@@ -1,17 +1,6 @@
-class CapitalRemittancesBulkActionJob < ApplicationJob
-  queue_as :low
-
-  def perform(capital_remittance_ids, user_id, bulk_action)
-    Chewy.strategy(:sidekiq) do
-      capital_remittances = CapitalRemittance.where(id: capital_remittance_ids)
-      capital_remittances.each do |doc|
-        perform_action(doc, user_id, bulk_action)
-      end
-    end
-
-    sleep(5)
-    msg = "#{bulk_action} completed for #{capital_remittance_ids.count} capital_remittances"
-    send_notification(msg, user_id, :success)
+class CapitalRemittancesBulkActionJob < BulkActionJob
+  def get_class
+    CapitalRemittance
   end
 
   def perform_action(capital_remittance, user_id, bulk_action)
