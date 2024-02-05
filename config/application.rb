f0a03f4611ed9c1c7d3245ed78db94b03c253dfb
thimmaiah <thimmaiah@gmail.com>
2024-02-05 23:09:08 +0530

    Misc small fixes

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module CapHive
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("indices")
 
-    core_view_paths = %w[access_rights documents entities folders notes permissions reminders tasks users investors investor_accesses valuations nudges base investor_kycs fees signature_workflows kyc_datas notifications]
+    core_view_paths = %w[access_rights documents entities folders notes permissions reminders tasks users investors investor_accesses valuations nudges base investor_kycs fees signature_workflows kyc_datas notifications core]
 
     core_view_paths.each do |view_path|
       config.paths['app/views'].unshift("#{Rails.root}/app/packs/core/#{view_path}/views")
