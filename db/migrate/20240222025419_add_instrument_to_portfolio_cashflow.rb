e4aedaf2e05d17f870338cc7e9e29e63827244ea
thimmaiah <thimmaiah@gmail.com>
2024-02-25 20:19:32 +0530

    Change to fix failing prod deployment

--- db/migrate/20240222025419_add_instrument_to_portfolio_cashflow.rb
+++ db/migrate/20240222025419_add_instrument_to_portfolio_cashflow.rb
@@ -1,7 +1,7 @@
 class AddInstrumentToPortfolioCashflow < ActiveRecord::Migration[7.1]
   def change
-    add_reference :portfolio_cashflows, :investment_instrument, null: true, foreign_key: true
+    # add_reference :portfolio_cashflows, :investment_instrument, null: true, foreign_key: true
     PortfolioCashflow.joins(:aggregate_portfolio_investment).update_all("portfolio_cashflows.investment_instrument_id=aggregate_portfolio_investments.investment_instrument_id") 
-    change_column_null :portfolio_cashflows, :investment_instrument_id, false
+    # change_column_null :portfolio_cashflows, :investment_instrument_id, false
   end
 end
