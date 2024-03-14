c2b344d4c2dd094555f1585fda504edd490e81d0
thimmaiah <thimmaiah@gmail.com>
2024-03-14 12:20:04 +0530

    Fixed offer spa generator

--- app/packs/secondaries/offers/models/offer_spa_generator.rb
+++ app/packs/secondaries/offers/models/offer_spa_generator.rb
@@ -30,24 +30,26 @@ class OfferSpaGenerator
 
     context = {}
     context.store  :effective_date, Time.zone.today.strftime("%d %B %Y")
-    context.store  :seller_offer_quantity, offer.quantity
-    context.store  :seller_company_name, offer.entity.name
+    context.store  :offer, offer
+    context.store  :sale_entity, offer.secondary_sale.entity
+    context.store  :offer_investor, offer.investor
+    context.store  :offer_user, offer.user
 
-    context.store  :seller_allocation_quantity, offer.allocation_quantity
-    allocation_quantity_in_words = offer.entity.currency == "INR" ? offer.allocation_quantity.to_i.rupees.humanize : offer.allocation_quantity.to_i.to_words.humanize
-    context.store :seller_allocation_quantity_words, allocation_quantity_in_words
+    context.store  :offer_allocation_quantity, offer.allocation_quantity
+    offer_allocation_quantity_in_words = offer.entity.currency == "INR" ? offer.allocation_quantity.to_i.rupees.humanize : offer.allocation_quantity.to_i.to_words.humanize
+    context.store :offer_allocation_quantity_words, offer_allocation_quantity_in_words
 
-    context.store  :seller_share_price, offer.secondary_sale.final_price
-    context.store  :seller_allocation_amount, offer.allocation_amount.to_f
+    context.store  :secondary_sale, offer.secondary_sale
+    context.store  :interest, offer.interest
+    context.store  :interest_investor, offer.interest.investor
 
     amount_in_words = offer.entity.currency == "INR" ? offer.allocation_amount.to_i.rupees.humanize : offer.allocation_amount.to_i.to_words.humanize
-    context.store :seller_allocation_amount_words, amount_in_words
+    context.store :offer_allocation_amount_words, amount_in_words
 
-    add_seller_fields(context, offer)
-    add_image(context, :seller_signature, offer.signature)
+    add_fees(context, offer)
 
-    add_buyer_fields(context, offer)
-    add_image(context, :buyer_signature, offer.interest&.signature)
+    add_image(context, :offer_signature, offer.signature)
+    add_image(context, :interest_signature, offer.interest&.signature)
 
     file_name = generated_file_name(offer)
     convert(template, context, file_name)
@@ -65,16 +67,7 @@ class OfferSpaGenerator
     add_header_footers(offer, file_name, additional_headers, additional_footers, template_document.name)
   end
 
-  def add_seller_fields(context, offer)
-    context.store  :seller_name, offer.full_name
-    context.store  :seller_address, offer.address
-    context.store  :seller_pan, offer.PAN
-    context.store  :seller_email, offer.user.email
-    context.store  :seller_bank_account_number, offer.bank_account_number
-    context.store  :seller_ifsc_code, offer.ifsc_code
-    context.store  :seller_demat, offer.demat
-    context.store  :seller_city, offer.city
-
+  def add_fees(context, offer)
     if offer.secondary_sale.fees
       fees = offer.compute_fees(offer.secondary_sale.fees)
       fee_amount = fees.sum(Money.new(0, offer.entity.currency)) { |i| i[:fee] }
@@ -83,26 +76,6 @@ class OfferSpaGenerator
       context.store  :net_allocation_amount, money_to_currency(offer.allocation_amount - fee_amount)
     end
 
-    offer.properties.each do |k, v|
-      context.store  "seller_#{k}", v
-    end
-
-    context.store "individual", %w[true yes 1].include?(offer.properties["individual"]&.downcase)
-  end
-
-  def add_buyer_fields(context, offer)
-    if offer.interest
-      context.store  :buyer_name, offer.interest.buyer_entity_name
-      context.store  :buyer_address, offer.interest.address
-      context.store  :buyer_email, offer.interest.email
-      context.store  :buyer_pan, offer.interest.PAN
-      context.store  :buyer_city, offer.interest.city
-      context.store  :buyer_demat, offer.interest.demat
-      context.store  :buyer_contact, offer.interest.contact_name
-
-      offer.interest.properties.each do |k, v|
-        context.store "buyer_#{k}", v
-      end
-    end
+    # context.store "individual", %w[true yes 1].include?(offer.properties["individual"]&.downcase)
   end
 end
