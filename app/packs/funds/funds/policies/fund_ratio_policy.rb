3dec165e55e50ae97c8e8872d8e0749a05d7be70
thimmaiah <thimmaiah@gmail.com>
2024-03-19 09:05:54 +0530

    Better fund ratio policy for investors

--- app/packs/funds/funds/policies/fund_ratio_policy.rb
+++ app/packs/funds/funds/policies/fund_ratio_policy.rb
@@ -1,10 +1,21 @@
 class FundRatioPolicy < FundBasePolicy
+  class Scope < FundBasePolicy::Scope
+    def resolve
+      if user.curr_role == "investor"
+        super.joins(:fund).where("funds.show_fund_ratios = true")
+      else
+        super
+      end
+    end
+  end
+
   def index?
     true
   end
 
   def show?
-    belongs_to_entity?(user, record)
+    belongs_to_entity?(user, record) ||
+      (record.fund.show_fund_ratios && permissioned_investor?)
   end
 
   def create?
