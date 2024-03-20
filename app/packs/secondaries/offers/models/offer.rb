41789a2e2ab00dbbac0195f988f0a8c43f99d9e2
thimmaiah <thimmaiah@gmail.com>
2024-03-20 11:02:17 +0530

    Removed FileIploader for PAN and Signature from offer

--- app/packs/secondaries/offers/models/offer.rb
+++ app/packs/secondaries/offers/models/offer.rb
@@ -43,8 +43,8 @@ class Offer < ApplicationRecord
 
   # has_many :messages, as: :owner, dependent: :destroy
 
-  include FileUploader::Attachment(:signature)
-  include FileUploader::Attachment(:pan_card)
+  # include FileUploader::Attachment(:signature)
+  # include FileUploader::Attachment(:pan_card)
   include FileUploader::Attachment(:spa)
 
   serialize :pan_verification_response, type: Hash
