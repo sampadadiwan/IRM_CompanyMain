cfb46f1bd5ed9ec58d99a10167ad34f686e80b73
thimmaiah <thimmaiah@gmail.com>
2024-03-22 19:05:11 +0530

    Added data room to secondaries

--- db/migrate/20240322131827_add_data_room_to_secondary_sale.rb
+++ db/migrate/20240322131827_add_data_room_to_secondary_sale.rb
@@ -1,6 +1,6 @@
 class AddDataRoomToSecondarySale < ActiveRecord::Migration[7.1]
   def up
-    # add_reference :secondary_sales, :data_room_folder, null: true, foreign_key: {to_table: :folders}
+    add_reference :secondary_sales, :data_room_folder, null: true, foreign_key: {to_table: :folders}
     SecondarySale.all.each do |s|
       puts "Creating data room for Secondary Sale #{s.name}"
       s.create_data_room
