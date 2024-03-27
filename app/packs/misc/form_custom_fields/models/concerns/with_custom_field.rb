d503c19faa67eb21909735ee06636473ca8e4490
thimmaiah <thimmaiah@gmail.com>
2024-03-27 09:31:12 +0530

    Added ability to add custom calcs to doc gen templates

--- app/packs/misc/form_custom_fields/models/concerns/with_custom_field.rb
+++ app/packs/misc/form_custom_fields/models/concerns/with_custom_field.rb
@@ -25,4 +25,25 @@ module WithCustomField
     OpenStruct.new(json_fields)
   end
   # rubocop:enable Style/OpenStructUse
+
+  def custom_calcs
+    CustomCalcs.new(self, custom_calculations)
+  end
+end
+
+# This is a class that is required for word templates which is used for document generation
+# It allows the template to access the custom calculations that are defined in the form type
+# Note templates use the sablon gem, that cannot call methods with params, hence we use the method_missing
+class CustomCalcs
+  attr_accessor :model, :custom_calcs
+
+  def initialize(model, custom_calcs)
+    @model = model
+    @custom_calcs = custom_calcs
+  end
+
+  def method_missing(method_name, *_args, &)
+    calc = @custom_calcs.find { |cf| cf.name == method_name.to_s }
+    @model.perform_custom_calculation(calc.meta_data)
+  end
 end
