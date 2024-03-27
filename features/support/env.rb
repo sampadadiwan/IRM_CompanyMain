4b6d1d3e632dcb55558963695824d5a504aa6154
thimmaiah <thimmaiah@gmail.com>
2024-03-28 00:53:55 +0530

    Now selenium tests run in full screen mode

--- features/support/env.rb
+++ features/support/env.rb
@@ -87,6 +87,7 @@ options.add_argument('--disable-popup-blocking')
 options.add_argument('--disable-translate')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--enable-features=NetworkService,NetworkServiceInProcess')
+options.add_argument('--window-size=1583,900')
 
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
