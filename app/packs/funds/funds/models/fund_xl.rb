416f8bc6f8dba0909bb4ef7dd3031bab5322ef3f
thimmaiah <thimmaiah@gmail.com>
2024-03-31 14:29:05 +0530

    Backup

--- app/packs/funds/funds/models/fund_xl.rb
+++ app/packs/funds/funds/models/fund_xl.rb
@@ -1,10 +1,3 @@
-require 'spreadsheet'
-require 'rubyXL/convenience_methods/cell'
-require 'rubyXL/convenience_methods/color'
-require 'rubyXL/convenience_methods/font'
-require 'rubyXL/convenience_methods/workbook'
-require 'rubyXL/convenience_methods/worksheet'
-
 class FundXl
   include FormTypeHelper
 
