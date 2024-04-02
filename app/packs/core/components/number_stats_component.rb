28c9f4d909b87e196bf5b0b15ceb6cea722e74f4
thimmaiah <thimmaiah@gmail.com>
2024-04-02 22:36:52 +0530

    Added fs_size as params

--- app/packs/core/components/number_stats_component.rb
+++ app/packs/core/components/number_stats_component.rb
@@ -1,11 +1,12 @@
 # app/components/fund_card_component.rb
 class NumberStatsComponent < ViewComponent::Base
-  def initialize(path:, amount:, subtitle:, progress_bar_color:)
+  def initialize(path:, amount:, subtitle:, progress_bar_color:, fs_size: 5)
     super
     @path = path
     @amount = amount
     @subtitle = subtitle
     @progress_bar_color = progress_bar_color
+    @fs_size = fs_size
   end
 
   attr_reader :path, :amount, :subtitle, :progress_bar_color
