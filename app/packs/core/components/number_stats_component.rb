dd4209b4c0a64b1efba5150c11167198fc118f61
Abhay Chauhan <ch.abhay@yahoo.com>
2024-04-10 19:12:17 +0530

    Component usage (#228)
    * User NumberStatsComponents in Secondary sales
    
    * Use NumberStatsComponents in Investors and portfolios
    
    * minor
    
    * Review change. Avoid a div if text info not present
    
    * NumberStatsComponent in funds stats
    
    * NumberStatsComponent in investor_kycs stats

--- app/packs/core/components/number_stats_component.rb
+++ app/packs/core/components/number_stats_component.rb
@@ -1,13 +1,14 @@
 # app/components/fund_card_component.rb
 class NumberStatsComponent < ViewComponent::Base
-  def initialize(path:, amount:, subtitle:, progress_bar_color:, fs_size: 5)
+  def initialize(path:, amount:, subtitle:, progress_bar_color:, fs_size: 5, text_info: nil)
     super
     @path = path
     @amount = amount
     @subtitle = subtitle
     @progress_bar_color = progress_bar_color
     @fs_size = fs_size
+    @text_info = text_info
   end
 
-  attr_reader :path, :amount, :subtitle, :progress_bar_color
+  attr_reader :path, :amount, :subtitle, :progress_bar_color, :text_info
 end
