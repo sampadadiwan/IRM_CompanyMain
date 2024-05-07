41a4f4978a54906866dfe1d7f1b225cf1129944e
thimmaiah <thimmaiah@gmail.com>
2024-05-07 21:02:47 +0530

    Changed fetch_esign_updates to post

--- config/routes/core.rb
+++ config/routes/core.rb
@@ -118,7 +118,7 @@ resources :documents do
   patch 'cancel_esign', on: :member
   patch 'send_all_for_esign', on: :collection
   post 'signature_progress', on: :collection
-  get 'fetch_esign_updates', on: :member
+  post 'fetch_esign_updates', on: :member
   get 'search', on: :collection
   get 'investor', on: :collection
   get 'folder', on: :collection
