78f0a96acff5d4b14700111dcfc169f8fad28206
thimmaiah <thimmaiah@gmail.com>
2024-05-17 20:26:34 +0530

    Added ability to run only SOAs

--- db/migrate/20240517144000_add_run_allocations_to_allocation_run.rb
+++ db/migrate/20240517144000_add_run_allocations_to_allocation_run.rb
@@ -1,5 +1,6 @@
 class AddRunAllocationsToAllocationRun < ActiveRecord::Migration[7.1]
   def change
     add_column :allocation_runs, :run_allocations, :boolean, default: true
+    AllocationRun.update_all(run_allocations: true)
   end
 end
