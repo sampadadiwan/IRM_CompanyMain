6cc763b8d0463aa6296b9f4143ea618510e0c7d8
thimmaiah <thimmaiah@gmail.com>
2024-05-18 15:00:50 +0530

    Added filtering by tags for funds

--- app/packs/funds/funds/controllers/funds_controller.rb
+++ app/packs/funds/funds/controllers/funds_controller.rb
@@ -5,6 +5,7 @@ class FundsController < ApplicationController
   def index
     @funds = policy_scope(Fund).includes(:entity)
     @funds = @funds.where(entity_id: params[:entity_id]) if params[:entity_id].present?
+    @funds = @funds.where("funds.tag_list LIKE ?", "%#{params[:tag]}%") if params[:tag].present?
     @funds = @funds.page(params[:page]) if params[:card].present? || params[:all].present?
   end
 
