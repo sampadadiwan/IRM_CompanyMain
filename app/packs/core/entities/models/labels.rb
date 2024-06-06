16bbf84d476a3894926456e2c2f9c361d4ad84ea
thimmaiah <thimmaiah@gmail.com>
2024-06-06 12:50:33 +0530

    Fixed investors kyc form for reg env

--- app/packs/core/entities/models/labels.rb
+++ app/packs/core/entities/models/labels.rb
@@ -2,16 +2,16 @@ class Labels
   LABEL_MAP = {
     SEBI: {
       'Tax ID': "PAN",
+      'Bank Routing Number': "IFSC Code",
       'Address Proof Documents': "PAN, Adhaar Card, Voter ID, Passport, Driving License"
     },
     DEFAULT: {
-      'Tax ID': "Tax ID",
       'Address Proof Documents': "Any valid address proof"
     }
   }.freeze
 
   def self.get(reg_env, key)
     map = LABEL_MAP[reg_env.to_sym] || LABEL_MAP[:DEFAULT]
-    map[key.to_sym]
+    map[key.to_sym] || key
   end
 end
