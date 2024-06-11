e5de9d96e41f932001153018e6e0b7db2fa2aaac
thimmaiah <thimmaiah@gmail.com>
2024-06-11 09:39:57 +0530

    Fixed issues with currency validation for PI create

--- app/packs/funds/portfolios/services/portfolio_investment_create.rb
+++ app/packs/funds/portfolios/services/portfolio_investment_create.rb
@@ -1,15 +1,45 @@
 class PortfolioInvestmentCreate < PortfolioInvestmentAction
+  step :validate_currency
+  left :handle_currency_errors, Output(:failure) => End(:failure)
   step :compute_amount_cents
   step :setup_aggregate
-  left :handle_setup_aggregate_errors
+  left :handle_setup_aggregate_errors, Output(:failure) => End(:failure)
   step :create_valuation
-  left :handle_valuation_errors
+  left :handle_valuation_errors, Output(:failure) => End(:failure)
   step :compute_fmv
   step :compute_quantity_as_of_date
   step :save
-  left :handle_errors
+  left :handle_errors, Output(:failure) => End(:failure)
   step :compute_avg_cost
   step :setup_attribution
+
+  # We need to ensure that the currency of the investment_instrument matches the currency of the fund.
+  # If it does not, we need to ensure that there is an exchange rate between the two.
+  # If there is no exchange rate, we cannot create the PortfolioInvestment and show the error
+  def validate_currency(_ctx, portfolio_investment:, **)
+    if portfolio_investment.investment_instrument.currency == portfolio_investment.fund.currency
+      # If the currencies are the same, we can proceed
+      true
+    else
+      from_currency = portfolio_investment.investment_instrument.currency
+      to_currency = portfolio_investment.fund.currency
+      er = ExchangeRate.where(from: from_currency, to: to_currency).last
+      if er
+        # If we have an exchange rate, we can proceed
+        true
+      else
+        # If we do not have an exchange rate, we cannot proceed
+        portfolio_investment.errors.add(:investment_instrument, "Exchange rate #{from_currency}->#{to_currency} not found")
+        false
+      end
+    end
+  end
+
+  def handle_currency_errors(ctx, portfolio_investment:, **)
+    Rails.logger.debug { "PortfolioInvestmentCreate Currency errors: #{portfolio_investment.errors.full_messages}" }
+    ctx[:errors] = portfolio_investment.errors.full_messages
+    false
+  end
 
   # After we save the PI, we need to create the attributions for sells.
   # When we import the data we create it in the same thread, as we need to ensure the attribution is setup before we move on to the next row. However if the portfolio_investment is created by the user, we can do it in the background.
@@ -47,5 +77,6 @@ class PortfolioInvestmentCreate < PortfolioInvestmentAction
   def handle_valuation_errors(ctx, valuation:, **)
     Rails.logger.debug { "PortfolioInvestmentCreate Valuation errors: #{valuation.errors.full_messages}" }
     ctx[:errors] = valuation.errors.full_messages
+    false
   end
 end
