fd5c7bb4a81d4c0394e7b105fd5374bc19f1ef46
thimmaiah <thimmaiah@gmail.com>
2024-06-12 21:14:07 +0530

    Fixed migration

--- db/migrate/20240612143400_add_transfer_qty_to_aggregate_portfolio_investment.rb
+++ db/migrate/20240612143400_add_transfer_qty_to_aggregate_portfolio_investment.rb
@@ -1,7 +1,7 @@
 class AddTransferQtyToAggregatePortfolioInvestment < ActiveRecord::Migration[7.1]
   def change
-    # add_column :aggregate_portfolio_investments, :transfer_amount_cents, :decimal, precision: 20, scale: 2, default: 0
-    # add_column :portfolio_investments, :transfer_amount_cents, :decimal, precision: 20, scale: 2, default: 0
+    add_column :aggregate_portfolio_investments, :transfer_amount_cents, :decimal, precision: 20, scale: 2, default: 0
+    add_column :portfolio_investments, :transfer_amount_cents, :decimal, precision: 20, scale: 2, default: 0
 
     # To ensure transfer_amount_cents rollups
     PortfolioInvestment.where("transfer_quantity > 0").each do |pi| 
