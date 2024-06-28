a7f2844d433d5bc47a3b3b44d4067974afe2c0a2
thimmaiah <thimmaiah@gmail.com>
2024-06-28 22:00:51 +0530

    Fixed commitment policy to disable support from update

--- app/packs/funds/capital_commitments/policies/capital_commitment_policy.rb
+++ app/packs/funds/capital_commitments/policies/capital_commitment_policy.rb
@@ -9,7 +9,7 @@ class CapitalCommitmentPolicy < FundBasePolicy
 
   def show?
     permissioned_employee? ||
-      permissioned_investor? 
+      permissioned_investor? || support?
   end
 
   def documents?
@@ -21,7 +21,7 @@ class CapitalCommitmentPolicy < FundBasePolicy
   end
 
   def update?
-    permissioned_employee?(:update)
+    permissioned_employee?(:update) 
   end
 
   def transfer_fund_units?
@@ -45,6 +45,6 @@ class CapitalCommitmentPolicy < FundBasePolicy
   end
 
   def destroy?
-    permissioned_employee?(:destroy)
+    permissioned_employee?(:destroy) || support?
   end
 end
