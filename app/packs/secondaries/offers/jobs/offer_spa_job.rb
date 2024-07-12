5568f68e3b612f7699411a2a6caaadb7e67febaa
Ausang Shulka <thimmaiah@gmail.com>
2024-07-12 13:05:21 +0530

    Added docs_to_generate custom field for offer SPA generation (#329)

--- app/packs/secondaries/offers/jobs/offer_spa_job.rb
+++ app/packs/secondaries/offers/jobs/offer_spa_job.rb
@@ -8,9 +8,20 @@ class OfferSpaJob < ApplicationJob
     Chewy.strategy(:sidekiq) do
       offer = Offer.find(offer_id)
       send_notification("Starting SPA generation for user #{offer.user}, Offer Id #{offer.id}", user_id, "info") if user_id.present?
+
       raise "No Offer Template found for Offer #{offer.id}" if offer.secondary_sale.documents.where(owner_tag: "Offer Template").blank?
 
-      offer.secondary_sale.documents.where(owner_tag: "Offer Template").find_each do |template|
+      # In special cases there is a custom field in the offer, docs_to_generate, that specifies which documents to generate. This is to enable some offers to have only one doc generated, but others to have more than one doc. E.x if the shares are in DEMAT, then only SPA needs to be generated, if not then SPA and another doc needs to be generated.
+      if offer.custom_fields.docs_to_generate.present?
+        template_names = offer.custom_fields.docs_to_generate.split(",")
+        templates = offer.secondary_sale.documents.where(owner_tag: "Offer Template").where(name: template_names)
+        Rails.logger.debug { "OfferSpaJob: Generating only docs_to_generate #{template_names}" }
+      else
+        templates = offer.secondary_sale.documents.where(owner_tag: "Offer Template")
+        Rails.logger.debug "OfferSpaJob: Generating all Offer Templates"
+      end
+
+      templates.find_each do |template|
         OfferSpaGenerator.new(offer, template)
         succeeded = true
       rescue StandardError => e
