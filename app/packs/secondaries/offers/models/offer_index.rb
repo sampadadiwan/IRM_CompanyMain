57f0c6ec9dad63f7a2bddb828459bfc387636e8a
thimmaiah <thimmaiah@gmail.com>
2024-08-29 20:29:47 +0530

    Removed holding from offer index

--- app/packs/secondaries/offers/models/offer_index.rb
+++ app/packs/secondaries/offers/models/offer_index.rb
@@ -1,7 +1,7 @@
 class OfferIndex < Chewy::Index
   SEARCH_FIELDS = %i[investor_name entity_name user_full_name acquirer_name PAN interest_entity_name investment_instrument custom_matching_vals].freeze
 
-  index_scope Offer.includes(:user, :investor, :secondary_sale, :entity, :holding, interest: :interest_entity)
+  index_scope Offer.includes(:user, :investor, :secondary_sale, :entity, interest: :interest_entity)
   field :entity_name, value: ->(h) { h.entity.name }
   field :investor_name, value: ->(h) { h.investor.investor_name if h.investor }
   field :entity_id
@@ -9,7 +9,6 @@ class OfferIndex < Chewy::Index
   field :secondary_sale_id
   field :acquirer_name
   field :custom_matching_vals
-  field :investment_instrument, value: ->(h) { h.holding.investment_instrument }
   field :PAN
   field :user_full_name, value: ->(h) { h.user.full_name if h.user }
   field :interest_entity_name, value: ->(h) { h.interest&.interest_entity&.name }
