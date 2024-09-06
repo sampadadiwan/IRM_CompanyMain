d9c2268363261fa2d1ce2bb4bb84b135088da97b
thimmaiah <thimmaiah@gmail.com>
2024-09-06 10:08:33 +0530

    Check interest price if final price is not specified for calc offer amounts

--- app/packs/secondaries/offers/models/offer.rb
+++ app/packs/secondaries/offers/models/offer.rb
@@ -99,8 +99,13 @@ class Offer < ApplicationRecord
 
     self.approved = false if quantity_changed?
 
-    self.amount_cents = quantity * final_price * 100 if final_price.positive?
-    self.allocation_amount_cents = allocation_quantity * final_price * 100 if final_price.positive?
+    if final_price.positive?
+      self.amount_cents = quantity * final_price * 100 
+      self.allocation_amount_cents = allocation_quantity * final_price * 100 
+    elsif self.interest.present?
+      self.amount_cents = quantity * interest.price * 100 
+      self.allocation_amount_cents = allocation_quantity * interest.price * 100  
+    end
     self.docs_uploaded_check ||= {}
     self.bank_verification_response ||= {}
     self.pan_verification_response ||= {}
