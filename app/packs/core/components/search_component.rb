d37aab889c33d394c61d117dd33d0dce33b14533
shrikant gour <shrikantgour018@gmail.com>
2024-10-07 10:38:51 +0530

    Call super - rubocop fix

--- app/packs/core/components/search_component.rb
+++ app/packs/core/components/search_component.rb
@@ -1,7 +1,7 @@
 class SearchComponent < ViewComponent::Base
-    def initialize(data_source:, turbo_frame:)
-        @data_source = data_source
-        @turbo_frame = turbo_frame
-        puts "SearchComponent#initialize with data_source: #{@data_source}"
-    end
+  def initialize(data_source:, turbo_frame:)
+    super
+    @data_source = data_source
+    @turbo_frame = turbo_frame
+  end
 end
