b483a97a71c0974fb2afcbf6d04c51e854a805d1
shrikantgour <48914363+shrikantgour@users.noreply.github.com>
2024-10-07 19:11:45 +0530

    Commitment search fix (#485)
    * Initial comitn for search comp fix
    
    * Table data fixes and investor views
    
    * Call super - rubocop fix
    
    * Page reset to 1 on search
    
    * Used routes for turbo frame
    
    * Added search helper to add hidden field inputs, moved styles to css, used proper turbo frame format
    
    * Used routes for turbo frame
    
    * Used routes for turbo frame
    
    * Fix pagination issue in turbo-frame
    
    * Search component change for investors
    
    * Fixed APIs search component
    
    * Search component change for investor_kyc
    
    * Search component change for investors
    
    * Added elastic search for PI, fixed search component for PI
    
    * Pass turbo frame to sort_link
    
    * Pass turbo frame to sort_link
    
    * Fix sorting issue on API index page
    
    * Pass turbo frame to sort_link
    
    * Fix sorting issue on PI index page, use turbo frame on instrument show page for PIs
    
    * Updated PI param in API details, commented Index import
    
    * Remove upload id drom API index, update API turbo frame name
    
    * Remove upload id init from API index view
    
    ---------
    
    Co-authored-by: thimmaiah <thimmaiah@gmail.com>
    Co-authored-by: Abhay <ch.abhay@yahoo.com>

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -4,14 +4,11 @@ export default class extends Controller {
     static targets = ["searchInput", "perPageInput", "submit"]
 
     connect() {
-        console.log("Search controller connected");
         this.debounceTimeout = null;
         this.previousSearchValue = this.searchInputTarget.value;
     }
 
     debouncedSearchInput(event) {
-        console.log("debouncedSearchInput called");
-        console.log(event.key);
 
         const isEnterKey = event.key === "Enter" || event.code === "Enter" || event.keyCode === 13;
         if (isEnterKey) {
@@ -23,7 +20,7 @@ export default class extends Controller {
         clearTimeout(this.debounceTimeout);
         this.debounceTimeout = setTimeout(() => {
             this.setSearchInput(event);
-        }, 750);
+        }, 900);
     }
 
     setSearchInput(event) {
@@ -31,29 +28,10 @@ export default class extends Controller {
         console.log(`setSearchInput called ${currentSearchValue}`);
 
         if (currentSearchValue === this.previousSearchValue && event.key !== "Enter") {
-            console.log("Search value has not changed. Skipping submission.");
             return;
         }
 
-        // let form = this.element;
-        let form = $("#search_form");
-        let actionUrl = window.location.href.split("?")[0];
-        console.log(actionUrl);
-        form.attr('action', actionUrl);
-        // form.setAttribute('action', actionUrl);
-
-        let query = window.location.search.substring(1);
-        let vars = query.split('&');
-
-        form.find("input[type='hidden']").remove(); // Remove old hidden inputs
-        vars.forEach(keyVal => {
-            console.log(`keyVal = ${keyVal}`);
-            let keyValArr = keyVal.split('=');
-            if (decodeURIComponent(keyValArr[0]) !== "search[value]" && decodeURIComponent(keyValArr[0]) !== "commit" && decodeURIComponent(keyValArr[0]) !== "page") {
-                form.append(`<input type='hidden' name='${keyValArr[0]}' value='${keyValArr[1]}' />`);
-            }
-        });
-
+        const form = $(event.target).closest('form.search-form'); // Using class
         // Make the call
         let submit = form.find("input[type='submit']");
         submit.off('click').click();
@@ -61,7 +39,8 @@ export default class extends Controller {
         this.previousSearchValue = currentSearchValue;
 
         // Show spinner and disable input
-        $("#search_spinner").show();
-        $("#search_input").prop("disabled", true);
+        // only closest to the form
+        form.find("#search_spinner").show();
+        form.find("#search_input").prop("disabled", true);
     }
 }
--- app/packs/core/components/search_component.rb
+++ app/packs/core/components/search_component.rb
@@ -1,2 +1,8 @@
 class SearchComponent < ViewComponent::Base
+  include SearchHelper
+  def initialize(data_source:, turbo_frame:)
+    super
+    @data_source = data_source
+    @turbo_frame = turbo_frame
+  end
 end
