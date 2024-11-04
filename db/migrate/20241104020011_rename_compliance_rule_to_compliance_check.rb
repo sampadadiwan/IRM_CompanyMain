2303bf1f0d9a7f302796d0512e72de8434a17d30
thimmaiah <thimmaiah@gmail.com>
2024-11-04 10:23:01 +0530

    Backup

--- db/migrate/20241104020011_rename_compliance_rule_to_compliance_check.rb
+++ db/migrate/20241104020011_rename_compliance_rule_to_compliance_check.rb
@@ -1,6 +1,6 @@
 class RenameComplianceRuleToComplianceCheck < ActiveRecord::Migration[7.1]
   def change
-    rename_table :ai_rules, :ai_rules
+    rename_table :compliance_rules, :ai_rules
     rename_table :compliance_checks, :ai_checks
     rename_column :ai_checks, :compliance_rule_id, :ai_rule_id
   end
