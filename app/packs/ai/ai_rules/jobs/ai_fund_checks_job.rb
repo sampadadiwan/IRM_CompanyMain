c60cade31e9e98fd42c411bc7ec321db1ab9f97c
thimmaiah <thimmaiah@gmail.com>
2024-11-10 13:44:34 +0530

    Now runs the rules actually

--- app/packs/ai/ai_rules/jobs/ai_fund_checks_job.rb
+++ app/packs/ai/ai_rules/jobs/ai_fund_checks_job.rb
@@ -9,11 +9,11 @@ class AiFundChecksJob < ApplicationJob
     for_classes.each do |for_class|
       if for_class == parent.class.to_s
         Rails.logger.debug { "Running checks for #{parent}, rule_type: #{rule_type}, schedule: #{schedule}" }
-        # AiChecksJob.perform_later(for_class, parent.id, user_id, rule_type, schedule)
+        AiChecksJob.perform_later(for_class, parent.id, user_id, rule_type, schedule)
       else
         parent.send(for_class.underscore.pluralize).each do |model|
           puts "Running checks for #{model}, rule_type: #{rule_type}, schedule: #{schedule}"
-          # AiChecksJob.perform_later(for_class, model.id, user_id, rule_type, schedule)
+          AiChecksJob.perform_later(for_class, model.id, user_id, rule_type, schedule)
         end
       end
     end
