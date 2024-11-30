5391f23377c5c6d66761b26aa879062c1e8284b6
Abhay Chauhan <ch.abhay@yahoo.com>
2024-11-30 10:09:58 +0530

    Revert "Import fund ratio (#612)"
    This reverts commit d3d5f8b93b788890f48f4b9e5be0f79cb784232d.

--- app/packs/funds/funds/services/import_fund_ratio_service.rb
+++
@@ -1,5 +0,0 @@
-class ImportFundRatioService < ImportServiceBase
-  step :read_file
-  step Subprocess(ImportFundRatio)
-  step :save_results_file
-end
--- db/migrate/20241128165107_add_import_upload_id_to_fund_ratio.rb
+++
@@ -1,5 +0,0 @@
-class AddImportUploadIdToFundRatio < ActiveRecord::Migration[7.2]
-  def change
-    add_column :fund_ratios, :import_upload_id, :bigint
-  end
-end
