adf2d9068cd42e079af5332151391d5df7cb761a
Abhay Chauhan <ch.abhay@yahoo.com>
2024-12-04 17:30:21 +0530

    Header cache fix (#626)

--- app/packs/misc/grid_view_preferences/model/grid_view_preference.rb
+++ app/packs/misc/grid_view_preferences/model/grid_view_preference.rb
@@ -1,8 +1,10 @@
 class GridViewPreference < ApplicationRecord
   belongs_to :owner, polymorphic: true
-  belongs_to :custom_grid_view, optional: true
   acts_as_list column: :sequence, scope: :owner
   validates :key, presence: true
+
+  after_save :touch_entity
+  after_destroy :touch_entity
 
   def self.get_column_name(parent, key)
     begin
@@ -13,5 +15,13 @@ class GridViewPreference < ApplicationRecord
     return column_name if column_name.present?
 
     key.gsub("custom_fields.", "").humanize
+  end
+
+  private
+
+  def touch_entity
+    if owner.is_a?(FormType) && owner.entity.present?
+      owner.entity.touch
+    end
   end
 end
