2a21f3ea04ccccc6287d0de1d3b8d35250273226
thimmaiah <thimmaiah@gmail.com>
2025-01-29 12:30:34 +0530

    Added filter for fund_id for account entries in KYC doc gen

--- app/packs/core/investor_kycs/services/kyc_doc_generator.rb
+++ app/packs/core/investor_kycs/services/kyc_doc_generator.rb
@@ -141,11 +141,12 @@ class KycDocGenerator
     }
 
     if fund_id.present?
-      fund_account_entries = AccountEntry.where(fund_id: fund_id).fund_entries
+      fund_account_entries = AccountEntry.fund_entries.where(fund_id: fund_id)
       context[:fund_account_entries] = TemplateDecorator.new(fund_account_entries)
       context[:fund_account_entries_between_dates] = TemplateDecorator.new(fund_account_entries.where(reporting_date: start_date..).where(reporting_date: ..end_date))
       context[:fund_account_entries_before_end_date] = TemplateDecorator.new(fund_account_entries.where(reporting_date: ..end_date))
     end
+
     context
   end
 
@@ -218,6 +219,9 @@ class KycDocGenerator
   # Add the reporting entries for the investor kyc, note that since a KYC can be linked to multiple commitments, there could be multiple account entries with the same name (ex Setup Fees, one for each commitment), so sum them (Ex Sum of Setup Fees) before adding to the context
   def add_reporting_entries(context, investor_kyc, start_date, end_date, fund_id)
     raes = investor_kyc.account_entries.where(reporting_date: start_date..end_date, rule_for: "Reporting")
+    # Filter by fund id if provided
+    raes = raes.where(fund_id: fund_id) if fund_id.present?
+
     first_commitment = investor_kyc.capital_commitments.first
     raes.group_by { |ae| [ae.name, ae.entry_type] }.each do |name_entry_type, aes|
       # Sum the amounts for the same name and entry type and create another dummy entry type
@@ -228,7 +232,7 @@ class KycDocGenerator
 
     if fund_id.present?
       # make sure to filter by fund id
-      fraes = AccountEntry.where(fund_id: fund_id, reporting_date: start_date..end_date, rule_for: "Reporting").fund_entries
+      fraes = AccountEntry.fund_entries.where(fund_id: fund_id, reporting_date: start_date..end_date, rule_for: "Reporting")
       fraes.group_by { |ae| [ae.name, ae.entry_type] }.each do |name_entry_type, aes|
         total_amount_cents = aes.sum(&:amount_cents)
         ae = AccountEntry.new(name: name_entry_type[0], entry_type: name_entry_type[1], amount_cents: total_amount_cents, fund_id: fund_id, reporting_date: aes[0].reporting_date)
