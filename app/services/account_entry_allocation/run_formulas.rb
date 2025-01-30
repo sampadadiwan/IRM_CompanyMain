4787cb2bcd711f1ebc86ae79aeb1a9a3a342b9e1
thimmaiah <thimmaiah@gmail.com>
2025-01-30 14:00:23 +0530

    Fix for generate SOAs without generating allocations

--- app/services/account_entry_allocation/run_formulas.rb
+++ app/services/account_entry_allocation/run_formulas.rb
@@ -13,56 +13,59 @@ module AccountEntryAllocation
     end
 
     def run_formulas(ctx, **)
-      return unless ctx[:run_allocations]
+      if ctx[:run_allocations]
 
-      fund                 = ctx[:fund]
-      rule_for             = ctx[:rule_for]
-      tag_list             = ctx[:tag_list]
-      user_id              = ctx[:user_id]
-      allocation_run       = ctx[:allocation_run]
-      start_date           = ctx[:start_date]
-      end_date             = ctx[:end_date]
-      run_start_time       = Time.zone.now
-      ctx[:bulk_insert_records] = []
+        fund                 = ctx[:fund]
+        rule_for             = ctx[:rule_for]
+        tag_list             = ctx[:tag_list]
+        user_id              = ctx[:user_id]
+        allocation_run       = ctx[:allocation_run]
+        start_date           = ctx[:start_date]
+        end_date             = ctx[:end_date]
+        run_start_time       = Time.zone.now
+        ctx[:bulk_insert_records] = []
 
-      # Get the enabled formulas
-      formulas = FundFormula.enabled.where(fund_id: fund.id).order(sequence: :asc)
-      formulas = formulas.where(rule_for: rule_for) if rule_for.present?
-      formulas = formulas.with_tags(tag_list.split(",")) if tag_list.present?
+        # Get the enabled formulas
+        formulas = FundFormula.enabled.where(fund_id: fund.id).order(sequence: :asc)
+        formulas = formulas.where(rule_for: rule_for) if rule_for.present?
+        formulas = formulas.with_tags(tag_list.split(",")) if tag_list.present?
 
-      ctx[:formula_count] = formulas.count
+        ctx[:formula_count] = formulas.count
 
-      formulas.each_with_index do |fund_formula, index|
-        ctx[:formula_index] = index
-        start_time = Time.zone.now
+        formulas.each_with_index do |fund_formula, index|
+          ctx[:formula_index] = index
+          start_time = Time.zone.now
 
-        # Call sub-operation to run the formula
-        # We re-use the same ctx for sub-ops, but add the current fund_formula:
-        sub_ctx = ctx.merge(fund_formula: fund_formula)
-        result = AccountEntryAllocation::RunFormula.wtf?(sub_ctx)
-        if result.failure? && result[:error].present?
-          # If there's an error, log/raise as needed
-          # The sub-operation might raise an error, or we can handle it here
-          raise result[:error]
+          # Call sub-operation to run the formula
+          # We re-use the same ctx for sub-ops, but add the current fund_formula:
+          sub_ctx = ctx.merge(fund_formula: fund_formula)
+          result = AccountEntryAllocation::RunFormula.wtf?(sub_ctx)
+          if result.failure? && result[:error].present?
+            # If there's an error, log/raise as needed
+            # The sub-operation might raise an error, or we can handle it here
+            raise result[:error]
+          end
+
+          # Update execution_time
+          fund_formula.update_column(:execution_time, ((Time.zone.now - start_time) * 1000).to_i)
+
+          # Provide notification
+          notify("Completed #{index + 1} of #{ctx[:formula_count]}: #{fund_formula.name}", :success, user_id)
+        rescue StandardError => e
+          error_message = "Error in Formula #{fund_formula.sequence}: #{fund_formula.name} : #{e.message}"
+          notify(error_message, :danger, user_id)
+          Rails.logger.debug { error_message }
+          allocation_run&.update_column(:status, error_message)
+          raise e
         end
 
-        # Update execution_time
-        fund_formula.update_column(:execution_time, ((Time.zone.now - start_time) * 1000).to_i)
-
-        # Provide notification
-        notify("Completed #{index + 1} of #{ctx[:formula_count]}: #{fund_formula.name}", :success, user_id)
-      rescue StandardError => e
-        error_message = "Error in Formula #{fund_formula.sequence}: #{fund_formula.name} : #{e.message}"
-        notify(error_message, :danger, user_id)
-        Rails.logger.debug { error_message }
-        allocation_run&.update_column(:status, error_message)
-        raise e
+        time_taken = ((Time.zone.now - run_start_time)).to_i
+        notify("Done running #{ctx[:formula_count]} formulas for #{start_date} - #{end_date} in #{time_taken} seconds", :success, user_id)
+        allocation_run&.update_column(:status, "Success")
+        true
+      else
+        true
       end
-
-      time_taken = ((Time.zone.now - run_start_time)).to_i
-      notify("Done running #{ctx[:formula_count]} formulas for #{start_date} - #{end_date} in #{time_taken} seconds", :success, user_id)
-      allocation_run&.update_column(:status, "Success")
-      true
     end
 
     # rubocop:disable Lint/RescueException
@@ -82,13 +85,14 @@ module AccountEntryAllocation
           false
         end
       else
+        Rails.logger.info "Skipping fund ratios generation"
         true
       end
     end
     # rubocop:enable Lint/RescueException
 
     # rubocop:disable Lint/RescueException
-    def generate_soa(ctx, fund:, start_date:, end_date:, user_id:, **)
+    def generate_soa(ctx, fund:, start_date:, end_date:, user_id:, template_id:, **)
       if ctx[:generate_soa]
         begin
           CapitalCommitmentSoaJob.perform_later(fund.id, nil, start_date.to_s, end_date.to_s, user_id, template_id:)
@@ -104,6 +108,7 @@ module AccountEntryAllocation
           false
         end
       else
+        Rails.logger.info "Skipping SOA generation"
         true
       end
     end
