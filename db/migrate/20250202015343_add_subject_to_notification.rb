9e467f3dda470a692d21666e6f06908052f76b22
thimmaiah <thimmaiah@gmail.com>
2025-02-03 15:32:05 +0530

    Uncommented out migration updates

--- db/migrate/20250202015343_add_subject_to_notification.rb
+++ db/migrate/20250202015343_add_subject_to_notification.rb
@@ -1,6 +1,6 @@
 class AddSubjectToNotification < ActiveRecord::Migration[7.2]
   def change
-    # add_column :noticed_notifications, :subject, :string
+    add_column :noticed_notifications, :subject, :string
     Noticed::Notification.all.each do |n|
       n.update_column(:subject, n.message.truncate(254))
     end
