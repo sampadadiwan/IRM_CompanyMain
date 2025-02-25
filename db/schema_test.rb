2f05fe31c80405f19d8db3b6204ef76aac52e768
thimmaiah <thimmaiah@gmail.com>
2025-02-25 05:59:41 +0530

    Backup

--- db/schema_test.rb
+++ db/schema_test.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_19_050454) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_23_001130) do
   create_table "access_rights", force: :cascade do |t|
     t.string "owner_type", null: false
     t.bigint "owner_id", null: false
