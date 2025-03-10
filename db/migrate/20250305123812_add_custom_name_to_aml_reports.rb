ecac73575e24c2a30104f49dbd053267cd7e9d91
thimmaiah <thimmaiah@gmail.com>
2025-03-10 20:54:37 +0530

    Added check for aml_report.investor_kyc in migration

--- db/migrate/20250305123812_add_custom_name_to_aml_reports.rb
+++ db/migrate/20250305123812_add_custom_name_to_aml_reports.rb
@@ -16,16 +16,20 @@ class AddCustomNameToAmlReports < ActiveRecord::Migration[7.2]
   def migrate_old_aml_reports
     AmlReport.find_each do |aml_report|
       Rails.logger.debug "Updating AML Report #{aml_report.id}"
-      aml_report.birth_date = aml_report.investor_kyc.birth_date if aml_report.birth_date.blank?
-      if aml_report.request_id.blank? && aml_report.response_data.present?
-        last_response = aml_report.response_data[aml_report.response_data.keys.last]
-        if last_response.instance_of?(Hash)
-          aml_report.request_id = last_response["request_id"]
-        elsif last_response.instance_of?(Array)
-          aml_report.request_id = last_response.first["request_id"]
+      if aml_report.investor_kyc.present?
+        aml_report.birth_date = aml_report.investor_kyc.birth_date if aml_report.birth_date.blank?
+        if aml_report.request_id.blank? && aml_report.response_data.present?
+          last_response = aml_report.response_data[aml_report.response_data.keys.last]
+          if last_response.instance_of?(Hash)
+            aml_report.request_id = last_response["request_id"]
+          elsif last_response.instance_of?(Array)
+            aml_report.request_id = last_response.first["request_id"]
+          end
         end
+        aml_report.save!
+      else
+        puts "AML Report #{aml_report.id} does not have Investor KYC"
       end
-      aml_report.save!
     end
   end
 
