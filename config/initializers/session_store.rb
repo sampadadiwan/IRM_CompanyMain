1bbee48f1ed2fa09fb0ac3a79cccfba1c195e4cb
thimmaiah <thimmaiah@gmail.com>
2025-03-10 08:59:58 +0530

    Added expiry of session and https cookies for VAPT

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,7 @@
-Rails.application.config.session_store :cookie_store, key: '_caphive_session', domain: :all, tld_length: 2 unless Rails.env.local?
+Rails.application.config.session_store :cookie_store, 
+  key: '_caphive_session', 
+  domain: :all, 
+  tld_length: 2, 
+  expire_after: 30.minutes, 
+  secure: !Rails.env.local?, 
+  httponly: true 
