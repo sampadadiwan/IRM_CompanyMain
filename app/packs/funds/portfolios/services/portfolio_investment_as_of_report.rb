e181ea6fe7bdcf830a350c1e246be82377ef7a6c
shrikant gour <shrikantgour018@gmail.com>
2025-04-08 15:20:13 +0530

    Updated downloaded excel files for PIs APIs and API as of report, minor n+1 fix

--- app/packs/funds/portfolios/services/portfolio_investment_as_of_report.rb
+++ app/packs/funds/portfolios/services/portfolio_investment_as_of_report.rb
@@ -16,8 +16,8 @@ class PortfolioInvestmentAsOfReport
     package = Axlsx::Package.new
     workbook = package.workbook
 
-    add_apis_tab(workbook)
     add_pis_tab(workbook)
+    add_apis_tab(workbook)
     add_portfolio_companies_tab(workbook)
 
     package
@@ -30,12 +30,12 @@ class PortfolioInvestmentAsOfReport
 
   def add_apis_tab(workbook)
     form_type, custom_field_names, custom_headers, custom_calcs, custom_calc_headers = get_form_type("AggregatePortfolioInvestment", entity_id: @current_user.entity_id)
-    inv_form_type, inv_custom_field_names, inv_custom_headers = get_form_type("Investor", entity_id: @current_user.entity_id)
     inv_inst_form_type, inv_inst_custom_field_names, inv_inst_custom_headers = get_form_type("InvestmentInstrument", entity_id: @current_user.entity_id)
 
     workbook.add_worksheet(name: "AggregatePortfolioInvestments") do |sheet|
       # Create the header row
-      sheet.add_row(["Fund", "Bought Quantity", "Bought Amount", "Sold Quantity", "Sold Amount", "Current Quantity", "FMV", "Cost", "Avg Cost / Share", "FIFO Cost"] + custom_headers + custom_calc_headers + ["Portfolio Company"] + inv_custom_headers + ["Instrument", "Category", "Sub Category", "Sector"] + inv_inst_custom_headers)
+
+      sheet.add_row(["Fund", "Portfolio Company", "Instrument", "Bought Quantity", "Bought Amount", "Avg Cost / Share", "Transfer Quantity", "Transfer Amount", "Sold Quantity", "Sold Amount", "FIFO Cost", "Realized Gain", "Current Quantity", "Cost of Remaining", "FMV", "Unrealized Gain"] + custom_headers + custom_calc_headers + ["Category", "Sub Category", "Sector"] + inv_inst_custom_headers)
 
       # Create entries for each item
       @aggregate_portfolio_investments.each do |api|
@@ -46,14 +46,12 @@ class PortfolioInvestmentAsOfReport
         custom_calc_values = get_custom_calc_values(api, form_type, custom_calcs)
 
         # We display the CFs for investor and investment_instrument
-        inv_custom_field_values = get_custom_values(api.portfolio_company, inv_form_type, inv_custom_field_names)
         inv_inst_custom_field_values = get_custom_values(api.investment_instrument, inv_inst_form_type, inv_inst_custom_field_names)
 
         # Add row to XL
-        sheet.add_row [api.fund.name, api.bought_quantity, api.bought_amount.to_d, api.sold_quantity, api.sold_amount.to_d, api.quantity, api.fmv, api.cost_of_remaining.to_d, api.avg_cost.to_d, api.cost_of_sold.to_d] +
+        sheet.add_row [api.fund.name, api.portfolio_company_name, api.investment_instrument&.name, api.bought_quantity, api.bought_amount.to_d, api.avg_cost.to_d, api.transfer_quantity, api.transfer_amount, api.sold_quantity, api.sold_amount.to_d, api.cost_of_sold.to_d, api.gain, api.quantity, api.cost_of_remaining.to_d, api.fmv, api.unrealized_gain] +
                       custom_field_values + custom_calc_values +
-                      [api.portfolio_company_name] + inv_custom_field_values +
-                      [api.investment_instrument&.name, api.investment_instrument&.category, api.investment_instrument&.sub_category, api.investment_instrument&.sector] + inv_inst_custom_field_values
+                      [api.investment_instrument&.category, api.investment_instrument&.sub_category, api.investment_instrument&.sector] + inv_inst_custom_field_values
       end
     end
   end
@@ -63,7 +61,7 @@ class PortfolioInvestmentAsOfReport
 
     workbook.add_worksheet(name: "PortfolioInvestments") do |sheet|
       # Create the header row
-      sheet.add_row(["Id", "Fund", "Portfolio Company", "Investment Date", "Amount", "Quantity", "Net Quantity", "Total Qty As Of Date", "Cost", "Cost Of Sold", "FMV", "Gain", "Unrealized Gain", "Instrument", "Notes"] + custom_headers + custom_calc_headers)
+      sheet.add_row(["Id", "Fund", "Portfolio Company", "Instrument", "Investment Date", "Quantity", "Amount", "Cost", "FMV", "Realized Gain", "Unrealized Gain", "Sold Quantity", "Transfer Quantity", "Net Quantity Available", "Total Qty As Of Investment Date", "Cost Of Sold", "Notes"] + custom_headers + custom_calc_headers)
 
       # Create entries for each item
       @aggregate_portfolio_investments.includes(portfolio_investments: %i[fund investment_instrument]).find_each do |api|
@@ -85,49 +83,65 @@ class PortfolioInvestmentAsOfReport
 
           pi = pi.as_of(@as_of) if @as_of.present? && @as_of != Time.zone.today
 
-          sheet.add_row [pi.id, pi.fund.name, pi.portfolio_company_name, pi.investment_date, pi.amount, pi.quantity, net_qty, pi.quantity_as_of_date, pi.cost, pi.cost_of_sold, pi.fmv, gain, unrealized_gain, pi.investment_instrument, pi.notes] + custom_field_values + custom_calc_values
+          sheet.add_row [pi.id, pi.fund.name, pi.portfolio_company_name, pi.investment_instrument, pi.investment_date, pi.quantity, pi.amount, pi.cost, pi.fmv, gain, unrealized_gain, pi.sold_quantity, pi.transfer_quantity, net_qty, pi.quantity_as_of_date, pi.cost_of_sold, pi.notes] + custom_field_values + custom_calc_values
         end
       end
     end
   end
 
+  # rubocop:disable Metrics/BlockLength
+  # rubocop:disable Metrics/MethodLength
   def add_portfolio_companies_tab(workbook)
     inv_form_type, inv_custom_field_names, inv_custom_headers = get_form_type("Investor", entity_id: @current_user.entity_id)
 
     workbook.add_worksheet(name: "Portfolio Company") do |sheet|
       # Create the header row
-      sheet.add_row(["Portfolio Company", "Bought Quantity", "Bought Amount", "Sold Quantity", "Sold Amount", "Current Quantity", "FMV", "Cost", "Avg Cost / Share", "Cost of Remaining"] + inv_custom_headers)
+      sheet.add_row(["Fund", "Portfolio Company", "Bought Quantity", "Bought Amount", "Avg Cost / Share", "Transfer Quantity", "Transfer Amount", "Sold Quantity", "Sold Amount", "FIFO Cost", "Realized Gain", "Current Quantity", "Cost of Remaining", "FMV", "Unrealized Gain"] + inv_custom_headers)
 
       # Create entries for each item
       @aggregate_portfolio_investments.group_by(&:portfolio_company).each do |pc, apis|
+        fund = nil
         bought_quantity = 0
         bought_amount = 0
         sold_quantity = 0
         sold_amount = 0
+        transfer_quantity = 0
+        transfer_amount = 0
         quantity = 0
         fmv = 0
         cost_of_remaining = 0
         avg_cost = 0
+        cost_of_sold = 0
+        gain = 0
+        unrealized_gain = 0
 
         apis.each do |api|
+          fund = api.fund
           api = api.as_of(@as_of) if @as_of.present? && @as_of != Time.zone.today
           bought_quantity += api.bought_quantity
           bought_amount += @bank.exchange_with(api.bought_amount, @currency)
           sold_quantity += api.sold_quantity
           sold_amount += @bank.exchange_with(api.sold_amount, @currency)
+          transfer_quantity += api.transfer_quantity
+          transfer_amount += @bank.exchange_with(api.transfer_amount, @currency)
           quantity += api.quantity
           fmv += @bank.exchange_with(api.fmv, @currency)
           cost_of_remaining += @bank.exchange_with(api.cost_of_remaining, @currency)
           avg_cost += @bank.exchange_with(api.avg_cost, @currency)
+          cost_of_sold += api.cost_of_sold
+          gain += api.gain
+          unrealized_gain += api.unrealized_gain
         end
 
         inv_custom_field_values = get_custom_values(pc, inv_form_type, inv_custom_field_names)
 
         # Add row to XL
-        sheet.add_row [pc.investor_name, bought_quantity, bought_amount.to_d, sold_quantity, sold_amount.to_d, quantity, fmv, cost_of_remaining.to_d, avg_cost.to_d, cost_of_remaining.to_d] + inv_custom_field_values
+        sheet.add_row [fund, pc.investor_name, bought_quantity, bought_amount.to_d, avg_cost.to_d, transfer_quantity, transfer_amount.to_d, sold_quantity, sold_amount.to_d, cost_of_sold.to_d, gain.to_d, quantity, cost_of_remaining.to_d, fmv, unrealized_gain.to_d] + inv_custom_field_values
       end
     end
   end
+  # rubocop:enable Metrics/BlockLength
+  # rubocop:enable Metrics/MethodLength
 
   def save_to_file(path)
     package = generate
