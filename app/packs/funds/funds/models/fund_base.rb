1564aa9cb109a93e21861ade1d0d1cba4d18b96d
thimmaiah <thimmaiah@gmail.com>
2025-04-13 07:56:14 +0530

    Added funds snapshots filter

--- app/packs/funds/funds/models/fund_base.rb
+++ app/packs/funds/funds/models/fund_base.rb
@@ -3,6 +3,7 @@ class FundBase < ApplicationRecord
   acts_as_favoritable
   include ForInvestor
   include WithCustomField
+  include RansackerAmounts.new(fields: %w[collected_amount committed_amount call_amount distribution_amount])
 
   CATEGORIES = ["Category I", "Category II", "Category III"].freeze
   REMITTANCE_GENERATION_BASIS = ["Folio Amount", "Fund Amount"].freeze
@@ -21,5 +22,13 @@ class FundBase < ApplicationRecord
 
   def has_tracking_currency?
     tracking_currency.present? && tracking_currency != currency
+  end
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[call_amount capital_commitments_count category collected_amount committed_amount currency distribution_amount first_close_date last_close_date name start_date tag_list unit_types snapshot_date].sort
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    []
   end
 end
