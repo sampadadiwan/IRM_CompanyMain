d4f7b95083713ab27f98d93f005e3278d6c009f7
thimmaiah <thimmaiah@gmail.com>
2025-04-20 21:12:10 +0530

    Refactored PortfolioInvestmentAsOfReport

--- db/migrate/20250420100001_add_base_numbers_to_portfolio_investment.rb
+++ db/migrate/20250420100001_add_base_numbers_to_portfolio_investment.rb
@@ -7,4 +7,11 @@ class AddBaseNumbersToPortfolioInvestment < ActiveRecord::Migration[8.0]
     add_column :portfolio_investments, :instrument_currency_unrealized_gain_cents, :decimal, precision: 20, scale: 2, default: 0
     add_column :aggregate_portfolio_investments, :instrument_currency_unrealized_gain_cents, :decimal, precision: 20, scale: 2, default: 0
   end
+
+  def post_deploy
+    PortfolioInvestment.all.each do |pi|
+      pi.compute_all_numbers
+      pi.save
+    end;nil
+  end
 end
