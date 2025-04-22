feef53e2195139cbcb3d6e23996afca9c4792b85
thimmaiah <thimmaiah@gmail.com>
2025-04-22 18:55:21 +0530

    Cached api_as_of for improving allocation speed

--- app/services/account_entry_allocation/allocate_aggregate_portfolios.rb
+++ app/services/account_entry_allocation/allocate_aggregate_portfolios.rb
@@ -16,26 +16,41 @@ module AccountEntryAllocation
 
       Rails.logger.debug { "allocate_aggregate_portfolios(#{fund_formula.name})" }
 
-      fund_formula.commitments(end_date, sample).each_with_index do |capital_commitment, idx|
-        # This is used to generate instance variables from the cached computed values
-        commitment_cache.computed_fields_cache(capital_commitment, start_date)
-        apis = fund.aggregate_portfolio_investments
-
-        apis.each do |orig_api|
-          ae = AccountEntry.new(
-            name: fund_formula.name,
-            entry_type: fund_formula.entry_type,
-            entity_id: fund.entity_id,
-            fund: fund,
-            reporting_date: end_date,
-            period: "As of #{end_date}",
-            generated: true,
-            fund_formula: fund_formula
-          )
-
-          # We create a "parent" reference
+      # We need the as_of for each api, check if we already have a cached copy
+      if ctx[:as_of_apis].present?
+        # Use the cached copy
+        as_of_apis = ctx[:as_of_apis]
+      else
+        as_of_apis = {}
+        fund.aggregate_portfolio_investments.each do |orig_api|
           api = orig_api.as_of(end_date) # aggregator object?
           Rails.logger.debug { "api: #{api}" }
+          as_of_apis[orig_api.id] = api
+        end
+        # Cache it for the next formula to use
+        ctx[:as_of_apis] = as_of_apis
+      end
+
+      fund.aggregate_portfolio_investments.each_with_index do |orig_api, idx|
+        # We setup the as_of api here from the cache of apis
+        api = as_of_apis[orig_api.id]
+
+        ae = AccountEntry.new(
+          name: fund_formula.name,
+          entry_type: fund_formula.entry_type,
+          entity_id: fund.entity_id,
+          fund: fund,
+          reporting_date: end_date,
+          period: "As of #{end_date}",
+          generated: true,
+          fund_formula: fund_formula
+        )
+
+        # We create a "parent" reference
+
+        fund_formula.commitments(end_date, sample).each do |capital_commitment|
+          # This is used to generate instance variables from the cached computed values
+          commitment_cache.computed_fields_cache(capital_commitment, start_date)
 
           begin
             create_instance_variables(ctx)
@@ -46,7 +61,7 @@ module AccountEntryAllocation
           end
         end
 
-        notify("Completed #{ctx[:formula_index] + 1} of #{ctx[:formula_count]}: #{fund_formula.name} : #{idx + 1} commitments", :success, user_id) if ((idx + 1) % 10).zero?
+        notify("Completed #{ctx[:formula_index] + 1} of #{ctx[:formula_count]}: #{fund_formula.name} : #{idx + 1} APIs", :success, user_id) if ((idx + 1) % 10).zero?
       end
 
       true
