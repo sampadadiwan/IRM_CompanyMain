231f429aa016c38e1a6f8e8882e343072235ed6f
thimmaiah <thimmaiah@gmail.com>
2025-04-23 20:54:23 +0530

    Backup

--- db/migrate/20250423045254_add_owner_to_chat.rb
+++ db/migrate/20250423045254_add_owner_to_chat.rb
@@ -1,5 +1,26 @@
 class AddOwnerToChat < ActiveRecord::Migration[8.0]
   def change
+
+    create_table :chats do |t|
+      t.string :model_id
+      t.references :entity, null: false, foreign_key: true # Entity the chat is associated with
+      t.references :user, null: false, foreign_key: true # User who initiated the chat
+      t.timestamps
+    end
+  
+    drop_table :messages, if_exists: true # Drop old table if it exists
+    create_table :messages do |t|
+      t.references :chat, null: false, foreign_key: true
+      t.string :role
+      t.text :content
+      # Optional fields for tracking usage/metadata
+      t.string :model_id
+      t.integer :input_tokens
+      t.integer :output_tokens
+      t.references :tool_call # Links tool result message to the initiating call
+      t.timestamps
+    end
+  
     add_reference :chats, :owner, polymorphic: true, null: true
     add_column :chats, :name, :string
   end
