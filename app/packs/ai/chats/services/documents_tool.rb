2d9438ac45154e01bd3f2d63dfea3d4bdaa34f3f
thimmaiah <thimmaiah@gmail.com>
2025-04-23 13:04:53 +0530

    BAckup

--- app/packs/ai/chats/services/documents_tool.rb
+++ app/packs/ai/chats/services/documents_tool.rb
@@ -1,9 +1,22 @@
 class DocumentsTool < RubyLLM::Tool
-  description "Gets the latest documents for the portfolio company"
-  param :portfolio_company_id, desc: "The id of the portfolio_company to get the latest documents for"
+    description "Searches for a document within the owner and returns it"
+  
+    attr_accessor :model
+    attr_accessor :chat
 
-  def execute(portfolio_company_id:)
-  rescue StandardError => e
-    { error: e.message }
-  end
+    param :query, desc: "The query by the user for the llm", required: true
+    param :name, desc: "The name of the document to be used in the query", required: true
+    def execute(query:, name:)
+      file_url = @model.documents.where("name like '%#{name}%'").first.file_url
+      @chat.ask(query, with: {pdf: file_url})
+    end
+
+    def setup(model:, chat:)
+      @model = model
+      @chat = chat
+      self
+    end
 end
+  
+# Let the AI use it
+# chat.with_tool(DocumentTool).ask "Find Investor Presentation document for the kpi report "
