213f126eb8997823736119a2cdd9c34d6a0752c0
shrikantgour <shrikantgour018@gmail.com>
2025-05-07 17:44:21 +0530

    Now searchbox can be hidden keeping per page

--- app/packs/core/components/search_component.rb
+++ app/packs/core/components/search_component.rb
@@ -1,8 +1,9 @@
 class SearchComponent < ViewComponent::Base
   include SearchHelper
-  def initialize(data_source:, turbo_frame:)
+  def initialize(data_source:, turbo_frame:, allow_search: true)
     super
     @data_source = data_source
     @turbo_frame = turbo_frame
+    @allow_search = allow_search
   end
 end
