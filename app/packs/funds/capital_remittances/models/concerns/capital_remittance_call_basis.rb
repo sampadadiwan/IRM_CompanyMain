f5f051ae486ec20e81d3894893f1865acf5153f7
shrikantgour <shrikantgour018@gmail.com>
2025-06-01 18:14:03 +0530

    Limit capital remittance percentage to -100.0 also

--- app/packs/funds/capital_remittances/models/concerns/capital_remittance_call_basis.rb
+++ app/packs/funds/capital_remittances/models/concerns/capital_remittance_call_basis.rb
@@ -77,7 +77,8 @@ module CapitalRemittanceCallBasis
 
     # Calculate the percentage of the committed folio amount that is being called.
     self.percentage = [(folio_call_amount_cents / capital_commitment.folio_committed_amount_cents) * 100.0, 100.0].min
-
+    # if percentage is less than -100 then limit to -100.0
+    self.percentage = -100.0 if percentage < -100.0
     logger.error "call_basis_upload: computed_amount_cents = #{computed_amount_cents}, \
                   call_amount_cents = #{call_amount_cents}, \
                   folio_call_amount_cents = #{folio_call_amount_cents} \
