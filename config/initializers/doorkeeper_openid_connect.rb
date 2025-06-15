443d1dfc6b14f6df999bd15441cdc67e1e132be4
Ausang Shukla <thimmaiah@gmail.com>
2025-06-15 16:56:12 +0530

    Added oidc key to staging

--- config/initializers/doorkeeper_openid_connect.rb
+++ config/initializers/doorkeeper_openid_connect.rb
@@ -14,6 +14,22 @@ Doorkeeper::OpenidConnect.configure do
     User.find_by(id: access_token.resource_owner_id)
   end
 
+  discovery_url_options do |request|
+
+    base_url = ENV["BASE_URL"]
+    # set the protocol based on the base_url protocol
+    protocol = base_url.start_with?("https") ? :https : :http
+    {
+      authorization: { protocol: }, # Or :http
+      token: { protocol: }, # Or :http
+      revocation: { protocol: }, # Or :http
+      introspection: { protocol: }, # Or :http
+      userinfo: { protocol: }, # Or :http
+      jwks: { protocol: }, # Or :http
+    }
+    
+  end
+
   auth_time_from_resource_owner do |resource_owner|
     # Return a timestamp indicating the last authentication time
     # This can be `resource_owner.current_sign_in_at` or similar
