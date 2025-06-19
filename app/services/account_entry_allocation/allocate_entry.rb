b7442ff3596e20102258c8fe013381c2ec6a5cda
Ausang Shukla <thimmaiah@gmail.com>
2025-06-19 16:06:15 +0530

    Backup

--- app/services/account_entry_allocation/allocate_entry.rb
+++ app/services/account_entry_allocation/allocate_entry.rb
@@ -31,7 +31,7 @@ module AccountEntryAllocation
 
         begin
           create_instance_variables(ctx)
-          AccountEntryAllocation::CreateAccountEntry.call(ctx.merge(account_entry: ae, capital_commitment: capital_commitment, parent: fund_account_entry, bdg: binding))
+          AccountEntryAllocation::CreateAccountEntry.call(ctx.merge(account_entry: ae, capital_commitment: capital_commitment, parent: fund_account_entry.freeze, bdg: binding))
         rescue StandardError => e
           raise "Error in #{fund_formula.name} for #{capital_commitment} #{fund_account_entry}: #{e.message}"
         end
