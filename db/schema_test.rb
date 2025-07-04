b0f65ff45365fae05774ffd2df452275b851af94
Ausang Shukla <thimmaiah@gmail.com>
2025-07-04 18:57:02 +0530

    Sync schema

--- db/schema_test.rb
+++ db/schema_test.rb
@@ -9,7 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema[8.0].define(version: 2025_07_03_120023) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_04_113707) do
   create_table "access_rights", force: :cascade do |t|
     t.string "owner_type", null: false
     t.bigint "owner_id", null: false
