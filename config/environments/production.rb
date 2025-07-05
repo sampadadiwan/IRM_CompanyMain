51c582c745ac7a5eeced59cb8ada7e80f2c021a0
Ausang Shukla <thimmaiah@gmail.com>
2025-07-05 18:24:20 +0530

    Changed production logs to info only

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [:id]
 
+  config.log_level = :info
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
