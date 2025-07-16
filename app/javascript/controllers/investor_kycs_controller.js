8b9c414a497b7a10de4ab71fd5f3efed391e7c88
Ausang Shukla <thimmaiah@gmail.com>
2025-07-16 19:35:56 +0530

    Added better kyc js to set Investing Entity to name for Individual KYC

--- app/javascript/controllers/investor_kycs_controller.js
+++ app/javascript/controllers/investor_kycs_controller.js
@@ -3,6 +3,25 @@ import  ServerDatatableController from "controllers/server_datatable_controller"
 
 export default class extends ServerDatatableController {
 
+  connect() {
+    // Initialize Select2 on the <select> element
+    this.initializeSelect2()
+  }
+
+  disconnect() {
+    // Clean up when navigating away
+    $(this.selectTarget).select2("destroy")
+  }
+
+  initializeSelect2() {
+    const el = this.selectTarget
+
+    // Listen for the internal state change and re-dispatch a native `change`
+    $(el).on("select2:select select2:unselect clear", () => {
+      el.dispatchEvent(new Event("change", { bubbles: true }))
+    })
+  }
+
   kyc_type_changed(event) {
     let kyc_type = $('#kyc_type').val();
     console.log("kyc_type", kyc_type);
@@ -13,5 +32,13 @@ export default class extends ServerDatatableController {
 
     window.location.href = href.toString();
   } 
+
+  investor_selected(event) {
+    // Selec the display value not the id 
+    let selectedOption = event.target.options[event.target.selectedIndex];
+    console.log("selectedOption", selectedOption);
+    // Set the full name input field with the selected option's text, only if the kyc type is individual
+    $("#individual_kyc_full_name").val(selectedOption.text);
+  }
   
 }
