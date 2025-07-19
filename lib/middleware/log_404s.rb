60d39ad7adfb8cd32f2d0b47825bdd98d61f5dbd
Ausang Shukla <thimmaiah@gmail.com>
2025-07-19 18:45:37 +0530

    Now handles MimeNegotiation::InvalidType for blacklists

--- lib/middleware/log_404s.rb
+++ lib/middleware/log_404s.rb
@@ -4,14 +4,25 @@ class Log404s
   end
 
   def call(env)
+    status = nil
+    headers = {}
+    response = []
+
     begin
       status, headers, response = @app.call(env)
+    rescue ActionDispatch::Http::MimeNegotiation::InvalidType => e
+      Rails.logger.warn "Log404s middleware: InvalidType error caught - #{e.message}"
+      # Set status to 404 to trigger the 404 logging block, but return 406 to the client
+      status = 404
+      headers = { 'Content-Type' => 'text/plain' }
+      response = ['406 Not Acceptable']
     rescue => e
       Rails.logger.error "Log404s middleware error: #{e.class} - #{e.message}"
       Rails.logger.error e.backtrace.join("\n")
       raise # Re-raise after logging
     end
 
+    # This block will now execute if status is 404 (either from app.call or from InvalidType rescue)
     if status == 404
       ip = env['REMOTE_ADDR']
       key = "rack::attack:404:#{ip}"
