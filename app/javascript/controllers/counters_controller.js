9b348a8f28efa5a6903b4927c772c99cc0124fec
thimmaiah <thimmaiah@gmail.com>
2025-12-05 07:31:23 +0530

    Added better CountUp for stats

--- app/javascript/controllers/counters_controller.js
+++ app/javascript/controllers/counters_controller.js
@@ -2,27 +2,56 @@ import { Controller } from "@hotwired/stimulus"
 import { CountUp } from 'countup.js';
 
 
-/* 
-<div data-controller="counters" data-counters-target-id-value="countup_id" 
-    data-counters-value-value="<%= some_value %>">
-    <span id="countup_id"></span>
-</div> 
+/*
+<div data-controller="counters"
+     data-counters-value-value="<%= 1234.56 %>"
+     data-counters-formatted-value-value="<%= number_to_currency(1234.56) %>">
+    <span data-counters-target="output"></span>
+</div>
 */
 
 export default class extends Controller {
 
+    static targets = [ "output" ]
     static values = {
-        targetId: String, // Which select id are we targeting
-        value: Number
+        value: Number,
+        formattedValue: String // The final formatted string (e.g., currency)
     }
 
   connect() {
+    const element = this.outputTarget;
+    const endVal = this.valueValue;
+    const formattedVal = this.formattedValueValue;
+    const duration = 2;
 
-    console.log(this.targetIdValue);
-    console.log(this.valueValue);
-    
-    const countUp = new CountUp(this.targetIdValue, this.valueValue);
-    countUp.start(() => console.log('Complete!'));
+    if (isNaN(endVal)) {
+      if (this.hasFormattedValueValue) element.innerHTML = formattedVal;
+      return;
+    }
+
+    const options = {
+      useGrouping: true,
+      separator: ',',
+      decimal: '.',
+      decimalPlaces: 0,
+      duration: duration,
+    };
+
+    const countUp = new CountUp(element, endVal, options);
+
+    if (!countUp.error) {
+      countUp.start();
+      if (this.hasFormattedValueValue) {
+        setTimeout(() => {
+          element.innerHTML = formattedVal;
+        }, duration * 1000 + 100); // 100ms buffer
+      }
+    } else {
+      console.error(countUp.error);
+      if (this.hasFormattedValueValue) {
+        element.innerHTML = formattedVal;
+      }
+    }
   }
 
 }
--- app/packs/core/components/number_stats_component.rb
+++ app/packs/core/components/number_stats_component.rb
@@ -1,10 +1,11 @@
 # app/components/fund_card_component.rb
 class NumberStatsComponent < ViewComponent::Base
   # rubocop :disable Metrics/ParameterLists
-  def initialize(path:, amount:, subtitle:, progress_bar_color:, fs_size: 5, text_info: nil, css_class: "")
+  def initialize(path:, amount:, subtitle:, progress_bar_color:, fs_size: 5, text_info: nil, css_class: "", raw_amount: nil)
     super()
     @path = path
     @amount = amount
+    @raw_amount = raw_amount
     @subtitle = subtitle
     @progress_bar_color = progress_bar_color
     @fs_size = fs_size
@@ -13,5 +14,5 @@ class NumberStatsComponent < ViewComponent::Base
   end
   # rubocop :enable Metrics/ParameterLists
 
-  attr_reader :path, :amount, :subtitle, :progress_bar_color, :text_info, :fs_size, :css_class
+  attr_reader :path, :amount, :raw_amount, :subtitle, :progress_bar_color, :text_info, :fs_size, :css_class
 end
